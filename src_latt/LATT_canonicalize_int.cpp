96bc45d57bad689e8caf4348d9ae4841a8dea316
Wessel van Woerden <wvw@cwi.nl>
2019-02-12 17:18:06 +0100

    Specialized canonical form function for integral matrices

---
+++ src_latt/LATT_canonicalize_int.cpp
@@ -0,0 +1,34 @@
+#include "NumberTheory.h"
+#include "MatrixCanonicalForm.h"
+
+int main(int argc, char *argv[])
+{
+  try {
+    if (argc != 3) {
+      std::cerr << "Number of argument is = " << argc << "\n";
+      std::cerr << "This program is used as\n";
+      std::cerr << "LATT_canonicalize [GramI] [GramO]\n";
+      std::cerr << "\n";
+      std::cerr << "GramI (input) : The gram matrix on input\n";
+      std::cerr << "GramO (output): The gram matrix on output\n";
+      return -1;
+    }
+    using T=mpq_class;
+    //    using T=mpz_class;
+    //    using Tint=mpz_class;
+    using Tint=long;
+    //
+    //    std::cerr << "Reading input\n";
+    std::ifstream is(argv[1]);
+    MyMatrix<Tint> eMat=ReadMatrix<Tint>(is);
+    MyMatrix<Tint> eMatCan=ComputeCanonicalForm<T,Tint>(eMat).second;
+    //
+    std::ofstream os(argv[2]);
+    WriteMatrix(os, eMatCan);
+    std::cerr << "Normal termination of LATT_canonicalize\n";
+  }
+  catch (TerminalException const& e) {
+    std::cerr << "Raised exception led to premature end of LATT_canonicalize\n";
+    exit(e.eVal);
+  }
+}
--- src_latt/MatrixCanonicalForm.h
+++ src_latt/MatrixCanonicalForm.h
@@ -9,6 +9,7 @@
 #include "MAT_MatrixInt.h"
 #include "MatrixGroup.h"
 
+//#include <time.h>
 
 
 template<typename T,typename Tint>
@@ -19,6 +20,9 @@ std::pair<MyMatrix<Tint>,MyMatrix<T>> ComputeCanonicalForm(MyMatrix<T> const& in
   //
   //  std::cerr << "inpMat=\n";
   //  WriteMatrix(std::cerr, inpMat);
+  //
+  //clock_t start = clock();
+
   MyMatrix<Tint> SHV = ExtractInvariantVectorFamilyZbasis<T,Tint>(inpMat);
   int nbRow=SHV.rows();
   int n=SHV.cols();
@@ -29,6 +33,7 @@ std::pair<MyMatrix<Tint>,MyMatrix<T>> ComputeCanonicalForm(MyMatrix<T> const& in
   }
 #endif
 
+  //std::cerr << "Generate SHV: " << double(clock()-start)/CLOCKS_PER_SEC << std::endl;
   
   //  std::cerr << "SHV=\n";
   //  WriteMatrix(std::cerr, SHV);
@@ -37,7 +42,10 @@ std::pair<MyMatrix<Tint>,MyMatrix<T>> ComputeCanonicalForm(MyMatrix<T> const& in
   //
   T TheTol=0;
   WeightMatrix<T,T> WMat = T_TranslateToMatrix_QM_SHV(inpMat, SHV, TheTol);
+  //std::cerr << "Generate inner products" << double(clock()-start)/CLOCKS_PER_SEC << std::endl;
   ReorderingSetWeight(WMat);
+
+  //std::cerr << "Generate weight matrix: " << double(clock()-start)/CLOCKS_PER_SEC << std::endl;
   //
   // Computing the canonicalization of the scalar product matrix
   //
@@ -45,6 +53,8 @@ std::pair<MyMatrix<Tint>,MyMatrix<T>> ComputeCanonicalForm(MyMatrix<T> const& in
   //  std::cerr << "We have PairCanonic\n";
   std::vector<int> MapVect = PairCanonic.first;
   std::vector<int> MapVectRev = PairCanonic.second;
+  
+  //std::cerr << "Get canonical order: " << double(clock()-start)/CLOCKS_PER_SEC << std::endl;
   //
   // Building the canonical basis
   //
@@ -66,8 +76,83 @@ std::pair<MyMatrix<Tint>,MyMatrix<T>> ComputeCanonicalForm(MyMatrix<T> const& in
   }
 #endif
   MyMatrix<T> RetMat = BasisCan_T * inpMat * TransposedMat(BasisCan_T);
+
+  //std::cerr << "Done: " << double(clock()-start)/CLOCKS_PER_SEC << std::endl;
   return {BasisCan_Tint, RetMat};
 }
+
+template<typename T,typename Tint>
+std::pair<MyMatrix<Tint>,MyMatrix<Tint>> ComputeCanonicalForm(MyMatrix<Tint> const& inpMat)
+{
+  //
+  // Computing the Z-basis on which the computation relies.
+  //
+  //  std::cerr << "inpMat=\n";
+  //  WriteMatrix(std::cerr, inpMat);
+  //
+  //clock_t start = clock();
+
+  MyMatrix<T> inpMat_T = ConvertMatrixUniversal<T,Tint>(inpMat);
+
+  MyMatrix<Tint> SHV = ExtractInvariantVectorFamilyZbasis<T,Tint>(inpMat_T);
+  int nbRow=SHV.rows();
+  int n=SHV.cols();
+#ifdef DEBUG
+  if (!CheckCentralSymmetry(SHV)) {
+    std::cerr << "The set of vector does not respect the central symmetry condition\n";
+    throw TerminalException{1};
+  }
+#endif
+
+  //std::cerr << "Generate SHV: " << double(clock()-start)/CLOCKS_PER_SEC << std::endl;
+  
+  //  std::cerr << "SHV=\n";
+  //  WriteMatrix(std::cerr, SHV);
+  //
+  // Computing the scalar product matrix
+  //
+  Tint TheTol=0;
+  WeightMatrix<Tint,Tint> WMat = T_TranslateToMatrix_QM_SHV(inpMat, SHV, TheTol);
+  //std::cerr << "Generate inner products" << double(clock()-start)/CLOCKS_PER_SEC << std::endl;
+  ReorderingSetWeight(WMat);
+
+  //std::cerr << "Generate weight matrix: " << double(clock()-start)/CLOCKS_PER_SEC << std::endl;
+  //
+  // Computing the canonicalization of the scalar product matrix
+  //
+  std::pair<std::vector<int>, std::vector<int>> PairCanonic = GetCanonicalizationVector<Tint,Tint,GraphBitset>(WMat);
+  //  std::cerr << "We have PairCanonic\n";
+  std::vector<int> MapVect = PairCanonic.first;
+  std::vector<int> MapVectRev = PairCanonic.second;
+  
+  //std::cerr << "Get canonical order: " << double(clock()-start)/CLOCKS_PER_SEC << std::endl;
+  //
+  // Building the canonical basis
+  //
+  MyMatrix<T> SHVcan_T(nbRow,n);
+  for (int iRowCan=0; iRowCan<nbRow; iRowCan++) {
+    int iRowNative = MapVectRev[iRowCan];
+    MyVector<Tint> eRow_Tint = GetMatrixRow(SHV, iRowNative);
+    MyVector<T> eRow_T = ConvertMatrixUniversal<T,Tint>(eRow_Tint);
+    AssignMatrixRow(SHVcan_T, iRowCan, eRow_T);
+  }
+  MyMatrix<T> BasisCan_T = GetZbasis(SHVcan_T);
+  MyMatrix<Tint> BasisCan_Tint = ConvertMatrixUniversal<Tint,T>(BasisCan_T);
+#ifdef DEBUG
+  T eDet = DeterminantMat(BasisCan_T);
+  T eDet_abs = T_abs(eDet);
+  if (eDet_abs != 1) {
+    std::cerr << "The matrix should be of determinant 1\n";
+    throw TerminalException{1};
+  }
+#endif
+  MyMatrix<Tint> RetMat = BasisCan_Tint * inpMat * TransposedMat(BasisCan_Tint);
+
+  //std::cerr << "Done: " << double(clock()-start)/CLOCKS_PER_SEC << std::endl;
+  return {BasisCan_Tint, RetMat};
+}
+
+
 
 
 #endif
