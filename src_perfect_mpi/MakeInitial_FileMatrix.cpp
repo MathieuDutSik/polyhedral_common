a9b5d50214a76eafe37df1bcba3aaa9a1ca5a4c3
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2019-05-24 09:31:32 +0200

    Some update for printing running times.
      Mathieu

--- src_perfect_mpi/MakeInitial_FileMatrix.cpp
+++ src_perfect_mpi/MakeInitial_FileMatrix.cpp
@@ -27,7 +27,6 @@ int main(int argc, char* argv[])
     os << nbPerfect << "\n";
     std::cerr << "nbPerfect=" << nbPerfect << "\n";
     for (int iPerfect=0; iPerfect<nbPerfect; iPerfect++) {
-      std::cerr << "iPerfect=" << iPerfect << " / " << nbPerfect << "\n";
       MyMatrix<Tint> ePerfect_Tint = ReadMatrix<Tint>(is);
       MyMatrix<T>    ePerfect_T = ConvertMatrixUniversal<T,Tint>(ePerfect_Tint);
       //
@@ -36,8 +35,13 @@ int main(int argc, char* argv[])
       Tshortest<T,Tint> eRec = T_ShortestVector<T,Tint>(ePerfect_T);
       int incd = (eRec.SHV.rows()) / 2;
       //
+      std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
       MyMatrix<T>    eMatCan_T = ComputeCanonicalForm<T,Tint>(ePerfect_T).second;
       MyMatrix<Tint> eMatCan_Tint = ConvertMatrixUniversal<Tint,T>(eMatCan_T);
+      std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+      int elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
+      std::cerr << "iPerfect=" << iPerfect << " / " << nbPerfect << " elapsed_seconds=" << elapsed_seconds << "\n";
+
       //
       os << eStatus << "\n";
       os << incd << "\n";
