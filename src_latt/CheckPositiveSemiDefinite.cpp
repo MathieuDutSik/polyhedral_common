f5cdee97f757ace4c4d95e8ab1c96588a7e46f05
Mathieu Dutour Sikiric <Mathieu.Dutour@gmail.com>
2020-05-07 06:43:48 +0200

    Insert code for positive definition.

---
+++ src_latt/CheckPositiveSemiDefinite.cpp
@@ -0,0 +1,26 @@
+#include "Temp_Positivity.h"
+int main(int argc, char *argv[])
+{
+  using T=mpq_class;
+  try {
+    if (argc != 2) {
+      fprintf(stderr, "Number of argument is = %d\n", argc);
+      fprintf(stderr, "This program is used as\n");
+      fprintf(stderr, "CheckPositiveSemiDefinite [inputMat]\n");
+      return -1;
+    }
+    // reading the matrix
+    std::ifstream INmat(argv[1]);
+    MyMatrix<T> TheMat=ReadMatrix<T>(INmat);
+    // computing the kernel
+    DiagSymMat<T> eDiag = DiagonalizeNonDegenerateSymmetricMatrix(TheMat);
+    if (eDiag.nbMinus > 0)
+      std::cout << "The matrix is NOT positive definite\n";
+    else
+      std::cout << "The matrix is positive definite\n";
+  }
+  catch (TerminalException const& e) {
+    exit(e.eVal);
+  }
+  std::cerr << "Normal termination of the program\n";
+}
