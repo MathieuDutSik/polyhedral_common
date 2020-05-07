514c622f8676fce54310b1c41ace1fbe254f704f
Mathieu Dutour Sikiric <Mathieu.Dutour@gmail.com>
2020-05-07 07:08:28 +0200

    Update the code.

--- src_latt/CheckPositiveSemiDefinite.cpp
+++ src_latt/CheckPositiveSemiDefinite.cpp
@@ -15,9 +15,13 @@ int main(int argc, char *argv[])
     // computing the kernel
     DiagSymMat<T> eDiag = DiagonalizeNonDegenerateSymmetricMatrix(TheMat);
     if (eDiag.nbMinus > 0)
-      std::cout << "The matrix is NOT positive definite\n";
-    else
-      std::cout << "The matrix is positive definite\n";
+      std::cout << "The matrix is NOT positive semidefinite\n";
+    else {
+      if (eDiag.nbZero > 0)
+        std::cout << "The matrix is positive semidefinite\n";
+      else
+        std::cout << "The matrix is positive definite\n";
+    }
   }
   catch (TerminalException const& e) {
     exit(e.eVal);
