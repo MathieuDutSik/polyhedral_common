6c06afc9c2591ebd00fb4c52285d6cd0b6497208
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2020-12-04 19:47:32 +0100

    Insert sample code.

---
+++ src_vinberg/VIN_FindSpecificDistance.cpp
@@ -0,0 +1,40 @@
+#include "MAT_Matrix.h"
+#include "NumberTheory.h"
+#include "MatrixCanonicalForm.h"
+#include "Temp_PolytopeEquiStab.h"
+
+
+int main(int argc, char* argv[])
+{
+  try {
+    if (argc != 2) {
+      std::cerr << "VIN_FindSpecificDistance [FileIn]\n";
+      throw TerminalException{1};
+    }
+    using T=mpz_class;
+    using Tint=mpz_class;
+
+    std::string FileIn  = argv[1];
+    std::ifstream is(FileIn);
+    //
+    ReadVector<T> G = ReadMatrix<T>(is);
+    ReadVector<T> v0 = ReadVector<T>(is);
+    VinbergInput<T,Tint> Vin{G, v0};
+    VinbergTot<T,Tint> Vtot = GetVinbergAux(Vin);
+    //
+    MyVector<Tint> a = ReadVector<Tint>(is);
+    T n;
+    is >> is;
+    std::vector<MyVector<Tint>> ListVect = Roots_decomposed_into(Vtot, a, n);
+    //
+    std::ostream os = std::cout;
+    int nVect = ListVect.size();
+    os << "|ListVect|=" << nVect << "\n";
+    for (int i=0; i<nVect; i++) {
+      WriteVector(os, ListVect[i]);
+    }
+  }
+  catch (TerminalException const& e) {
+    exit(e.eVal);
+  }
+}
