4806a6499bcd541dfff47f4ac3adffff7d18ceeb
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2020-12-16 03:42:01 -0500

    Insertion of the debugging text.

---
+++ src_ctype_mpi/CTYP_debug.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <boost/mpi/environment.hpp>
+#include <boost/mpi/communicator.hpp>
+
+int main()
+{
+  boost::mpi::environment env;
+  boost::mpi::communicator world;
+  size_t irank=world.rank();
+  if (irank == 0) {
+    std::vector<int> V(100, 0);
+    boost::mpi::request req = world.isend(1, 37, V);
+    while(true) {
+      boost::optional<boost::mpi::status> stat = req.test();
+      if (stat) {
+	if (stat->error() != 0) {
+	  std::cerr << "Rank 0: Non-zero error in stat\n";
+          char error_string[10000];
+          int length_of_error_string;
+          MPI_Error_string(stat->error(), error_string, &length_of_error_string);
+          fprintf(stderr, "err: %s\n", error_string);
+	  return 1;
+	}
+	break;
+      }
+    }
+  } else {
+    while(true) {
+      boost::optional<boost::mpi::status> prob = world.iprobe();
+      if (prob) {
+	if (prob->tag() == 37) {
+	  std::vector<int> V;
+	  world.recv(prob->source(), prob->tag(), V);
+	  std::cerr << "Rank 1: Correct receiving of the vector\n";
+	  break;
+	}
+      }
+    }
+  }
+}
