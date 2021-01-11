e77fe4a56c34238faff2b50d94996ddc86038a89
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-01-11 14:54:35 +0100

    Now we pass without error.

--- src_ctype_mpi/CTYP_CheckAdjacency.cpp
+++ src_ctype_mpi/CTYP_CheckAdjacency.cpp
@@ -19,16 +19,19 @@ int main(int argc, char* argv[])
   size_t n_pes=n_pes_i;
   //
   std::string Prefix = argv[1];
-  std::string PrefixAdj = argv[1];
+  std::string PrefixAdj = argv[2];
+  std::cerr << "Prefix=" << Prefix << " PrefixAdj=" << PrefixAdj << "\n";
   std::vector<int> ListAdj_Glob;
   std::vector<int> ListNbCtype(n_pes);
   std::vector<int> ListNbAdj_Glob;
   // Reading number of adjacencies
   for (size_t i_pes=0; i_pes<n_pes; i_pes++) {
     std::string eFile = Prefix + std::to_string(i_pes) + ".nc";
+    std::cerr << "eFile=" << eFile << "\n";
     netCDF::NcFile dataFile(eFile, netCDF::NcFile::read);
-    netCDF::NcVar varNbAdj=dataFile.getVar("idx_proc");
+    netCDF::NcVar varNbAdj=dataFile.getVar("nb_adjacent");
     size_t n_ctype = varNbAdj.getDim(0).getSize();
+    std::cerr << "n_ctype=" << n_ctype << "\n";
     ListNbCtype[i_pes] = n_ctype;
     std::vector<int> ListNbAdj_Loc(n_ctype);
     std::vector<size_t> start{0};
@@ -37,6 +40,7 @@ int main(int argc, char* argv[])
     for (auto & eVal : ListNbAdj_Loc)
       ListNbAdj_Glob.push_back(eVal);
   }
+  std::cerr << "We have ListNbAdj_Glob\n";
   int n_ctype_glob = std::accumulate(ListNbCtype.begin(), ListNbCtype.end(), 0);
   std::vector<int> ListNbAdjShift_Glob(n_ctype_glob);
   ListNbAdjShift_Glob[0] = 0;
@@ -48,12 +52,17 @@ int main(int argc, char* argv[])
   // Reading the adjacencies themselves
   for (size_t i_pes=0; i_pes<n_pes; i_pes++) {
     std::string eFileAdj = PrefixAdj + std::to_string(i_pes) + ".nc";
+    std::cerr << "eFileAdj=" << eFileAdj << "\n";
     netCDF::NcFile dataFileAdj(eFileAdj, netCDF::NcFile::read);
     netCDF::NcVar varIdxProc=dataFileAdj.getVar("idx_proc");
     netCDF::NcVar varIdxAdjacent=dataFileAdj.getVar("idx_adjacent");
     size_t n_adjacencies = varIdxProc.getDim(0).getSize();
     std::vector<int8_t> ListIdxProc_Loc(n_adjacencies);
     std::vector<int8_t> ListIdxAdjacent_Loc(n_adjacencies);
+    std::vector<size_t> start{0};
+    std::vector<size_t> count{n_adjacencies};
+    varIdxProc.getVar(start, count, ListIdxProc_Loc.data());
+    varIdxAdjacent.getVar(start, count, ListIdxAdjacent_Loc.data());
     for (size_t i=0; i<n_adjacencies; i++) {
       int8_t iProc = ListIdxProc_Loc[i];
       int iAdj_Loc = ListIdxAdjacent_Loc[i];
@@ -77,10 +86,13 @@ int main(int argc, char* argv[])
     int i_glob = ListShiftNbCtype[irank] + i_loc;
     int nbadj = ListNbAdj_Glob[i_glob];
     int shift = ListNbAdjShift_Glob[i_glob];
+    std::cerr << "i_loc=" << i_loc << " i_glob=" << i_glob << " nbadj=" << nbadj << " shift=" << shift << "\n";
     for (int iadj=0; iadj<nbadj; iadj++) {
       int eAdj = ListAdj_Glob[shift + iadj];
-      if (!IsVerticesAdjacent(eAdj, i_glob))
+      if (!IsVerticesAdjacent(eAdj, i_glob)) {
+        std::cerr << "  eAdj=" << eAdj << " i_glob=" << i_glob << "\n";
         nb_error++;
+      }
     }
   }
   std::cerr << "nb_error=" << nb_error << "\n";
