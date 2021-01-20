5b38973c29f5e852b8d475657d630e80a6bf058a
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-01-20 15:47:38 +0100

    Insertion of code for enumerating regions of hyperplane arrangements.

---
+++ src_poly/POLY_GapFindHyperplaneRegions.cpp
@@ -0,0 +1,50 @@
+#include "NumberTheory.h"
+#include "POLY_Hyperplane.h"
+
+int main(int argc, char *argv[])
+{
+  try {
+    if (argc != 3) {
+      std::cerr << "Number of argument is = " << argc << "\n";
+      std::cerr << "This program is used as\n";
+      std::cerr << "POLY_redundancy [DATAIN] [DATAOUT]\n";
+      std::cerr << "\n";
+      std::cerr << "DATAIN : The polyhedral cone inequalities\n";
+      std::cerr << "DATAOUT : The list of irredundant facets\n";
+      return -1;
+    }
+    //
+    std::ifstream is(argv[1]);
+    using T=mpq_class;
+    MyMatrix<T> ListV=ReadMatrix<T>(is);
+    //
+    std::vector<Face> ListFace = EnumerateHyperplaneRegions(ListV);
+    int n_vect = ListV.rows();
+    std::cerr << "len(ListFace)=" << ListFace.size() << "\n";
+    //
+    std::ofstream os(argv[2]);
+    os << "return [";
+    int nbFace=ListFace.size();
+    for (int i=0; i<nbFace; i++) {
+      if (i>0)
+        os << ",\n";
+      //
+      Face f = ListFace[i];
+      std::vector<int> f_vect;
+      for (int i_vect=0; i_vect<n_vect; i_vect++)
+        if (f[i_vect])
+          f_vect.push_back(i_vect+1);
+      os << "[";
+      for (size_t u=0; u<f_vect.size(); u++) {
+        if (u>0)
+          os << ",";
+        os << f_vect[u];
+      }
+      os << "]";
+    }
+    os << "];\n";
+  }
+  catch (TerminalException const& e) {
+    exit(e.eVal);
+  }
+}
--- src_poly/POLY_Hyperplane.h
+++ src_poly/POLY_Hyperplane.h
@@ -1,22 +1,79 @@
 #ifndef INCLUDE_POLY_HYPERPLANE
 #define INCLUDE_POLY_HYPERPLANE
 
+#include "POLY_cddlib.h"
+#include <unordered_set>
 
 
-
-
-
+// The list of region is described by a vector of + and -.
+// If it is + then the encoding is by a 1. Otherwise it is by a 0.
 template<typename T>
 std::vector<Face> EnumerateHyperplaneRegions(MyMatrix<T> const& ListV)
 {
-  int n=
+  int n=ListV.cols();
+  int n_vect=ListV.rows();
   auto GetSingleEntry=[&]() -> Face {
-     while(true) {
-       
-     }
+    auto try_vect=[&](MyVector<T> const& V) -> std::pair<bool,Face> {
+      Face f(n_vect);
+      for (int i_vect=0; i_vect<n_vect; i_vect++) {
+        T eScal=0;
+        for (int i=0; i<n; i++)
+          eScal += V(i) * ListV(i_vect,i);
+        if (eScal == 0)
+          return {false,f};
+        if (eScal > 0)
+          f[i_vect] = 1;
+      }
+      return {true,f};
+    };
+    while(true) {
+      MyVector<T> eV(n);
+      for (int i=0; i<n; i++) {
+        int eVal = rand() % 10;
+        eV(i) = eVal;
+      }
+      std::pair<bool,Face> ePair = try_vect(eV);
+      if (ePair.first)
+        return ePair.second;
+    }
   };
-
-
-
-  
+  std::unordered_set<Face> ListDone;
+  std::unordered_set<Face> ListUndone;
+  ListUndone.insert(GetSingleEntry());
+  auto fInsert=[&](Face const& f) -> void {
+    if (ListDone.count(f) == 0)
+      return;
+    ListUndone.insert(f);
+  };
+  auto ProcessAdjacent=[&](Face const& eF) -> void {
+    MyMatrix<T> ListInequalities(n_vect,n);
+    for (int i_vect=0; i_vect<n_vect; i_vect++) {
+      int eSign=-1;
+      if (eF[i_vect])
+        eSign=1;
+      for (int i=0; i<n; i++)
+        ListInequalities(i_vect,i) = eSign * ListV(i_vect,i);
+    }
+    std::vector<int> ListIrred = cdd::RedundancyReductionClarkson(ListInequalities);
+    for (auto & idx : ListIrred) {
+      Face newF = eF;
+      newF[idx] = 1 - eF[idx];
+      fInsert(newF);
+    }
+  };
+  while(true) {
+    if (ListUndone.size() == 0)
+      break;
+    Face f = *(ListUndone.begin());
+    ProcessAdjacent(f);
+    ListUndone.erase(f);
+    ListDone.insert(f);
+  }
+  std::vector<Face> ListFace;
+  for (auto & eF : ListDone)
+    ListFace.push_back(eF);
+  return ListFace;
 }
+
+
+#endif
