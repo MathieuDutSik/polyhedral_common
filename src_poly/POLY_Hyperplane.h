88dd27304740163ac61e0a4825acea440a02d338
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-01-20 15:01:28 +0100

    some insert.

---
+++ src_poly/POLY_Hyperplane.h
@@ -0,0 +1,22 @@
+#ifndef INCLUDE_POLY_HYPERPLANE
+#define INCLUDE_POLY_HYPERPLANE
+
+
+
+
+
+
+template<typename T>
+std::vector<Face> EnumerateHyperplaneRegions(MyMatrix<T> const& ListV)
+{
+  int n=
+  auto GetSingleEntry=[&]() -> Face {
+     while(true) {
+       
+     }
+  };
+
+
+
+  
+}
