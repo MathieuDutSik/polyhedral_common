07b3c5f2c5cb4cf854a7f4f137ede2291ab188a9
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-05-18 17:48:37 +0200

    More movement of the code.

--- src_poly/PERM_Fct.h
+++ src_poly/PERM_Fct.h
@@ -33,6 +33,79 @@ std::vector<Tidx> SortingPerm(std::vector<T> const & ListV)
 
 
 
+template<typename T, typename Telt>
+MyMatrix<T> RepresentVertexPermutation(MyMatrix<T> const& EXT1, MyMatrix<T> const& EXT2, Telt const& ePerm)
+{
+  SelectionRowCol<T> eSelect=TMat_SelectRowCol(EXT1);
+  std::vector<int> ListRowSelect=eSelect.ListRowSelect;
+  MyMatrix<T> M1=SelectRow(EXT1, ListRowSelect);
+  MyMatrix<T> M1inv=Inverse(M1);
+  size_t nbRow=ListRowSelect.size();
+  std::vector<int> ListRowSelectImg(nbRow);
+  for (size_t iRow=0; iRow<nbRow; iRow++)
+    ListRowSelectImg[iRow]=ePerm.at(iRow);
+  MyMatrix<T> M2=SelectRow(EXT2, ListRowSelectImg);
+  return M1inv*M2;
+}
+
+
+
+template<typename T, typename Telt>
+Telt GetPermutationOnVectors(MyMatrix<T> const& EXT1, MyMatrix<T> const& EXT2)
+{
+  using Tidx = typename Telt::Tidx;
+  size_t nbVect=EXT1.rows();
+  std::vector<MyVector<T>> EXTrow1(nbVect), EXTrow2(nbVect);
+  for (size_t iVect=0; iVect<nbVect; iVect++) {
+    EXTrow1[iVect]=GetMatrixRow(EXT1, iVect);
+    EXTrow2[iVect]=GetMatrixRow(EXT2, iVect);
+  }
+  Telt ePerm1=Telt(SortingPerm<MyVector<T>,Tidx>(EXTrow1));
+  Telt ePerm2=Telt(SortingPerm<MyVector<T>,Tidx>(EXTrow2));
+  Telt ePermRet=(~ePerm1) * ePerm2;
+#ifdef DEBUG
+  for (size_t iVect=0; iVect<nbVect; iVect++) {
+    size_t jVect=ePermRet.at(iVect);
+    if (EXTrow2[jVect] != EXTrow1[iVect]) {
+      std::cerr << "iVect=" << iVect << " jVect=" << jVect << "\n";
+      std::cerr << "Id:";
+      for (size_t k=0; k<nbVect; k++)
+	std::cerr << " " << k;
+      std::cerr << "\n";
+      std::cerr << " p:";
+      for (size_t k=0; k<nbVect; k++)
+	std::cerr << " " << ePermRet.at(k);
+      std::cerr << "\n";
+
+      std::cerr << "perm1:";
+      for (size_t k=0; k<nbVect; k++)
+	std::cerr << " " << ePerm1.at(k);
+      std::cerr << "\n";
+      std::cerr << "perm2:";
+      for (size_t k=0; k<nbVect; k++)
+	std::cerr << " " << ePerm2.at(k);
+      std::cerr << "\n";
+
+
+      std::cerr << "EXTrow1[iVect]=";
+      WriteVector(std::cerr, EXTrow1[iVect]);
+      std::cerr << "EXTrow2[jVect]=";
+      WriteVector(std::cerr, EXTrow2[jVect]);
+      std::cerr << "EXTrow1=\n";
+      WriteMatrix(std::cerr, EXT1);
+      std::cerr << "EXTrow2=\n";
+      WriteMatrix(std::cerr, EXT2);
+      std::cerr << "Error in GetPermutationOnVectors\n";
+      throw TerminalException{1};
+    }
+  }
+#endif
+  return ePermRet;
+}
+
+
+
+
 
 
 
--- src_poly/Temp_PolytopeEquiStab.h
+++ src_poly/Temp_PolytopeEquiStab.h
@@ -3110,8 +3110,9 @@ std::vector<std::vector<unsigned int>> GetListGenAutomorphism_ListMat_Subset(MyM
 
 
 template<typename T>
-EquivTest<std::vector<unsigned int>> TestEquivalence_ListMat_Subset(MyMatrix<T> const& EXT1, std::vector<MyMatrix<T>> const&ListMat1, Face const& eSubset1,
-                                                                    MyMatrix<T> const& EXT2, std::vector<MyMatrix<T>> const&ListMat2, Face const& eSubset2)
+EquivTest<std::vector<unsigned int>> TestEquivalence_ListMat_Subset(
+                                       MyMatrix<T> const& EXT1, std::vector<MyMatrix<T>> const&ListMat1, Face const& eSubset1,
+                                       MyMatrix<T> const& EXT2, std::vector<MyMatrix<T>> const&ListMat2, Face const& eSubset2)
 {
 #ifdef TIMINGS
   std::chrono::time_point<std::chrono::system_clock> time1 = std::chrono::system_clock::now();
@@ -3326,7 +3327,7 @@ std::vector<std::vector<unsigned int>> LinPolytopeAntipodalIntegral_Automorphism
 #endif
 #ifdef TIMINGS
   std::chrono::time_point<std::chrono::system_clock> time6 = std::chrono::system_clock::now();
-  std::cerr << "|*_GetListGenerators|=" << std::chrono::duration_cast<std::chrono::microseconds>(time6 - time5).count() << "\n";
+  std::cerr << "|GetListGenerators|=" << std::chrono::duration_cast<std::chrono::microseconds>(time6 - time5).count() << "\n";
 #endif
   return ListGen;
 }
@@ -3337,79 +3338,6 @@ std::vector<std::vector<unsigned int>> LinPolytopeAntipodalIntegral_Automorphism
 
 
 
-
-
-
-
-template<typename T, typename Telt>
-MyMatrix<T> RepresentVertexPermutation(MyMatrix<T> const& EXT1, MyMatrix<T> const& EXT2, Telt const& ePerm)
-{
-  SelectionRowCol<T> eSelect=TMat_SelectRowCol(EXT1);
-  std::vector<int> ListRowSelect=eSelect.ListRowSelect;
-  MyMatrix<T> M1=SelectRow(EXT1, ListRowSelect);
-  MyMatrix<T> M1inv=Inverse(M1);
-  size_t nbRow=ListRowSelect.size();
-  std::vector<int> ListRowSelectImg(nbRow);
-  for (size_t iRow=0; iRow<nbRow; iRow++)
-    ListRowSelectImg[iRow]=ePerm.at(iRow);
-  MyMatrix<T> M2=SelectRow(EXT2, ListRowSelectImg);
-  return M1inv*M2;
-}
-
-
-
-template<typename T, typename Telt>
-Telt GetPermutationOnVectors(MyMatrix<T> const& EXT1, MyMatrix<T> const& EXT2)
-{
-  using Tidx = typename Telt::Tidx;
-  size_t nbVect=EXT1.rows();
-  std::vector<MyVector<T>> EXTrow1(nbVect), EXTrow2(nbVect);
-  for (size_t iVect=0; iVect<nbVect; iVect++) {
-    EXTrow1[iVect]=GetMatrixRow(EXT1, iVect);
-    EXTrow2[iVect]=GetMatrixRow(EXT2, iVect);
-  }
-  Telt ePerm1=Telt(SortingPerm<MyVector<T>,Tidx>(EXTrow1));
-  Telt ePerm2=Telt(SortingPerm<MyVector<T>,Tidx>(EXTrow2));
-  Telt ePermRet=(~ePerm1) * ePerm2;
-#ifdef DEBUG
-  for (size_t iVect=0; iVect<nbVect; iVect++) {
-    size_t jVect=ePermRet.at(iVect);
-    if (EXTrow2[jVect] != EXTrow1[iVect]) {
-      std::cerr << "iVect=" << iVect << " jVect=" << jVect << "\n";
-      std::cerr << "Id:";
-      for (size_t k=0; k<nbVect; k++)
-	std::cerr << " " << k;
-      std::cerr << "\n";
-      std::cerr << " p:";
-      for (size_t k=0; k<nbVect; k++)
-	std::cerr << " " << ePermRet.at(k);
-      std::cerr << "\n";
-
-      std::cerr << "perm1:";
-      for (size_t k=0; k<nbVect; k++)
-	std::cerr << " " << ePerm1.at(k);
-      std::cerr << "\n";
-      std::cerr << "perm2:";
-      for (size_t k=0; k<nbVect; k++)
-	std::cerr << " " << ePerm2.at(k);
-      std::cerr << "\n";
-
-
-      std::cerr << "EXTrow1[iVect]=";
-      WriteVector(std::cerr, EXTrow1[iVect]);
-      std::cerr << "EXTrow2[jVect]=";
-      WriteVector(std::cerr, EXTrow2[jVect]);
-      std::cerr << "EXTrow1=\n";
-      WriteMatrix(std::cerr, EXT1);
-      std::cerr << "EXTrow2=\n";
-      WriteMatrix(std::cerr, EXT2);
-      std::cerr << "Error in GetPermutationOnVectors\n";
-      throw TerminalException{1};
-    }
-  }
-#endif
-  return ePermRet;
-}
 
 
 
