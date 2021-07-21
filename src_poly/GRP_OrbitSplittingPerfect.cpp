d837d955ebb8981027f220d9c08d3994fd5883a5
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-07-21 10:27:28 +0200

    Correction by switching to a more modern group library.

--- src_poly/GRP_OrbitSplittingPerfect.cpp
+++ src_poly/GRP_OrbitSplittingPerfect.cpp
@@ -1,5 +1,8 @@
-#include "Permlib_specific.h"
+#include "Permutation.h"
+#include "Group.h"
 #include "GRP_GroupFct.h"
+
+
 int main(int argc, char *argv[])
 {
   try {
@@ -16,7 +19,10 @@ int main(int argc, char *argv[])
       return -1;
     }
     //
-    using Tgroup=TheGroupFormat<mpz_class>;
+    using Tidx = uint16_t;
+    using Telt = permutalib::SingleSidedPerm<Tidx>;
+    using Tint = mpz_class;
+    using Tgroup = permutalib::Group<Telt,Tint>;
     std::cerr << "Reading input\n";
     //
     std::ifstream is1(argv[1]);
