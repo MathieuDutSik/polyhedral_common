10dd8db082425fedcb94a0041c8ecc1f980f4b1a
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-07-31 12:28:56 +0200

    More code insert.

--- src_dualdesc/POLY_RecursiveDualDesc.h
+++ src_dualdesc/POLY_RecursiveDualDesc.h
@@ -1022,9 +1022,23 @@ vectface DUALDESC_AdjacencyDecomposition(
 
 
 struct message_facet {
-  int e_hash;
+  size_t e_hash;
   vectface vf; // List of vectface by the DatabaseBank
 };
+
+struct message_query {
+  size_t e_hash;
+  uint8_t query;
+  // 0: for TerminationInfo
+  // 1: For destroying the databank and sending back the vectface
+};
+
+template<typename Tint>
+struct HashUndoneOrbitInfo {
+  size_t e_hash;
+  UndoneOrbitInfo<Tint> erec;
+};
+
 
 namespace boost::serialization {
 
@@ -1033,6 +1047,21 @@ namespace boost::serialization {
   {
     ar & make_nvp("hash", mesg.e_hash);
     ar & make_nvp("vf", mesg.vf);
+  }
+
+  template<class Archive>
+  inline void serialize(Archive & ar, message_query & mesg, const unsigned int version)
+  {
+    ar & make_nvp("hash", mesg.e_hash);
+    ar & make_nvp("query", mesg.query);
+  }
+
+  template<class Archive, typename Tint>
+  inline void serialize(Archive & ar, HashUndoneOrbitInfo<Tint> & mesg, const unsigned int version)
+  {
+    ar & make_nvp("hash", mesg.e_hash);
+    ar & make_nvp("nbundone", mesg.erec.nbUndone);
+    ar & make_nvp("setundone", mesg.erec.eSetUndone);
   }
 
 }
@@ -1094,49 +1123,129 @@ vectface MPI_DUALDESC_AdjacencyDecomposition(
   int irank=comm.rank();
   int size=comm.size();
   // New Facets (processed asynchronously)
-  const int tag_new_facets = 37; // New facets to be added, the most common request
-  // Balinski style premature termination (processed as early as possible)
-  const int tag_request_status_enum = 38; // Request for the Balinski stuff
-  const int tag_terminate_enumeration = 39; // Request to terminate enumeration as everything is matching
-  const int tag_termination_confirmation = 40; // The enumeration is confirmed to have been terminated.
+  const int tag_new_facets = 36; // New facets to be added, the most common request
+  // Message query being received
+  const int tag_message_query = 37;
+  // undone information for Balinski termination
+  const int tag_nbundone_balinski = 38;
+  // undone information for Balinski termination
+  const int tag_terminate_send_vf = 38;
 
   size_t n_rows = EXT.rows();
   std::vector<size_t> subset_index_proc = get_subset_index_rev(n_rows);
-
+  struct BigRecordEntry {
+    std::vector<size_t> subset_index_proc;
+    DatabaseOrbits<T,Tint,Tgroup> databank;
+  };
 
   // We can have DatabseBank created on disjoint processes.
   // The order will not be the same between processors.
-  std::vector<DatabaseOrbits<T,Tint,Tgroup>> ListRPL;
-  ListRPL.emplace_back(DatabaseOrbits<T,Tint,Tgroup>(EXT, GRP, ePrefix, AllArr.Saving, std::cerr));
-  std::unordered_map<int,size_t> map; // mapping from the hash of database orbit to the index in ListRPL
-  std::vector<int> map_rev; // mapping from index of ListRPL to hash
-  map[0] = 0;
-  map_rev.push_back(0);
+  std::vector<BigRecordEntry> ListRPL;
+  ListRPL.emplace_back({get_subset_index_rev(EXT.rows()), DatabaseOrbits<T,Tint,Tgroup>(EXT, GRP, ePrefix, AllArr.Saving, std::cerr)});
+  std::unordered_map<size_t,uint8_t> map_databank; // mapping from the hash of database orbit to the index in ListRPL
+  std::vector<size_t> map_databank_rev; // mapping from index of ListRPL to hash
+  map_databank[0] = 0;
+  map_databank_rev.push_back(0);
+  auto get_pos=[&](auto & x) -> uint8_t {
+    return map_databank[x.e_hash];
+  };
+  auto remove_database_entry=[&](const size_t& e_hash, const uint8_t& pos) -> void {
+    // Correct ListRPL
+    ListRPL.erase(pos);
+    // Correct map_databank
+    map_databank.erase(e_hash);
+    for (auto & kv : map_databank) {
+      if (kv.second > pos)
+        map_databank[kv.first] = kv.second - 1;
+    }
+    // Correct map_databank
+    for (size_t i=pos+1; i<map_databank_rev.size(); i++)
+      map_databank_rev[i-1] = map_databank_rev[i];
+    map_databank_rev.pop_back();
+  };
+  
   // The Buffers in output and receive
   std::vector<message_facet> ListEntries_OUT(size);
   std::vector<message_facet> ListEntries_IN;
+  // The queries
+  std::vector<std::pair<int,message_query>> ListMesgQuery;
+  // The entry nbundone
+  std::vector<std::pair<int,HashUndoneOrbitInfo<Tint>>> ListMesgUndone;
+  // The partial dual desc
+  std::vector<std::pair<int,message_facet>> ListMesgPartDualDesc;
   // The MPI related stuff
   std::vector<boost::mpi::request> ListRequest;
   std::vector<int> RequestStatus;
+  auto GetFreeIndex=[&]() -> size_t {
+    size_t len = RequestStatus.size();
+    for (size_t i=0; i<len; i++) {
+      if (RequestStatus[i] == 1) {
+        boost::optional<boost::mpi::status> stat = ListRequest[i].test();
+        if (stat) { // that request has ended. Let's read it.
+          RequestStatus[i] = 0;
+        }
+      }
+    }
+    for (size_t i=0; i<len; i++)
+      if (RequestStatus[i] == 0)
+        return i;
+    ListRequest.push_back(boost::mpi::request());
+    RequestStatus.push_back(1);
+    return len;
+  };
   // The infinite loop to do the enumeration
   while (true) {
     boost::optional<boost::mpi::status> prob = comm.iprobe();
     if (prob) {
-      std::cerr << "We are probing something\n";
-      if (prob->tag() == tag_new_facets) { // proessed asynchronously
+      // We only do data reception processing is done afterwards
+      if (prob->tag() == tag_new_facets) {
         message_facet e_mesg;
         comm.recv(prob->source(), prob->tag(), e_mesg);
         ListEntries_IN.push_back(e_mesg);
       }
-    } else {
-      // First clearing the buffers
-      for (auto & eEntry_IN : ListEntries_IN) {
-        int e_hash = eEntry_IN.e_hash;
-        size_t pos = map[e_hash];
-        for (auto & eFace : eEntry_IN.vf)
-          ListRPL[pos].FuncInsert(eFace);
+      if (prob->tag() == tag_message_query) {
+        message_query e_mesg;
+        comm.recv(prob->source(), prob->tag(), e_mesg);
+        ListMesgQuery.push_back({prob->source(), e_mesg});
       }
-      // Now treating the block with the smallest
+      if (prob->tag() == tag_nbundone_balinski) {
+        HashUndoneOrbitInfo<Tint> e_mesg;
+        comm.recv(prob->source(), prob->tag(), e_mesg);
+        ListMesgUndone.push_back({prob->source(), e_mesg});
+      }
+      if (prob->tag() == tag_terminate_send_vf) {
+        message_facet e_mesg;
+        comm.recv(prob->source(), prob->tag(), e_mesg);
+        ListMesgPartDualDesc.push_back({prob->source(), std::move(e_mesg)});
+      }
+    } else {
+      // First clearing the buffers of facets
+      for (auto & eEntry_IN : ListEntries_IN) {
+        uint8_t pos = get_pos(eEntry_IN);
+        for (auto & eFace : eEntry_IN.vf)
+          ListRPL[pos].databank.FuncInsert(eFace);
+      }
+      ListEntries_IN.clear();
+      // Processing the message queries
+      for (auto & eMesgQuery : ListMesgQuery) {
+        uint8_t pos = get_pos(eMesgQuery.second);
+        if (eMesgQuery.second.query == 0) {
+          UndoneOrbitInfo<Tint> recundone = ListRPL[pos].databank.GetTerminationInfo();
+          HashUndoneOrbitInfo<Tint> hashrecundone{eMesgQuery.second.e_hash, recundone};
+          size_t u = GetFreeIndex();
+          ListRequest[u] = comm.isend(0, tag_nbundone_balinski, hashrecundone);
+          RequestStatus[u] = 1;
+        }
+        if (eMesgQuery.second.query == 1) {
+          message_facet e_mesg{eMesgQuery.second.e_hash, ListRPL[pos].databank.FuncListOrbitIncidence()};
+          size_t u = GetFreeIndex();
+          ListRequest[u] = comm.isend(0, tag_terminate_send_vf, e_mesg);
+          RequestStatus[u] = 1;
+          remove_database_entry(eMesgQuery.second.e_hash, pos);
+        }
+      }
+      ListMesgQuery.clear();
+      // Now treating the block with the smallest size
     }
   }
   return ListRPL[0].FuncListOrbitIncidence();
