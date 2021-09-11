848f829cd111a0619578e53fe85c75f520746851
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-09-11 12:22:08 +0200

    Some changes.

--- src_poly/POLY_PolytopeInt.h
+++ src_poly/POLY_PolytopeInt.h
@@ -4,8 +4,8 @@
 #include "COMB_Combinatorics.h"
 
 
-template<typename T>
-std::vector<MyVector<T>> GetListIntegralPoint(MyMatrix<T> const& FAC, MyMatrix<T> const& EXT)
+template<typename T, typename Tint, typename Finsert>
+void Kernel_GetListIntegralPoint(MyMatrix<T> const& FAC, MyMatrix<T> const& EXT, Finsert f_insert)
 {
   int nbVert=EXT.rows();
   int n=EXT.cols();
@@ -17,24 +17,22 @@ std::vector<MyVector<T>> GetListIntegralPoint(MyMatrix<T> const& FAC, MyMatrix<T
     for (int i=0; i<n; i++)
       EXTnorm(iVert,i)=eValInv*EXT(iVert,i);
   }
-  std::vector<T> ListLow(dim);
-  std::vector<T> ListUpp(dim);
+  std::vector<Tint> ListLow(dim);
+  std::vector<Tint> ListUpp(dim);
   std::vector<int> ListSize(dim);
   for (int iDim=0; iDim<dim; iDim++) {
     MyVector<T> eCol(nbVert);
     for (int iVert=0; iVert<nbVert; iVert++)
-      eCol(iVert)=EXTnorm(iVert,iDim+1);
-    T eLow=Ceil(eCol.minCoeff());
-    T eUpp=Floor(eCol.maxCoeff());
-    ListLow[iDim]=eLow;
-    ListUpp[iDim]=eUpp;
-    T eVal_T=1 + eUpp - eLow;
-    int eVal_I=UniversalScalarConversion<int,T>(eVal_T);
-    ListSize[iDim]=eVal_I;
+      eCol(iVert) = EXTnorm(iVert,iDim+1);
+    Tint eLow=Ceil(eCol.minCoeff());
+    Tint eUpp=Floor(eCol.maxCoeff());
+    ListLow[iDim] = eLow;
+    ListUpp[iDim] = eUpp;
+    Tint eSiz=1 + eUpp - eLow;
+    ListSize[iDim]=eSiz;
   }
-  BlockIterationMultiple BlIter(ListSize);
+  int nbFac=FAC.rows();
   auto IsCorrect=[&](MyVector<T> const& eVect) -> bool {
-    int nbFac=FAC.rows();
     for (int iFac=0; iFac<nbFac; iFac++) {
       T eScal=0;
       for (int i=0; i<n; i++)
@@ -44,18 +42,48 @@ std::vector<MyVector<T>> GetListIntegralPoint(MyMatrix<T> const& FAC, MyMatrix<T
     }
     return true;
   };
-  std::vector<MyVector<T>> ListPoint;
+  MyVector<Tint> ePoint(n);
+  ePoint(0)=1;
+  BlockIterationMultiple BlIter(ListSize);
   for (auto const& eVect : BlIter) {
-    MyVector<T> ePoint(n);
-    ePoint(0)=1;
     for (int iDim=0; iDim<dim; iDim++)
-      ePoint(iDim+1)=ListLow[iDim] + eVect[iDim];
+      ePoint(iDim+1) = ListLow[iDim] + eVect[iDim];
     bool test=IsCorrect(ePoint);
     if (test)
-      ListPoint.push_back(ePoint);
+      f_insert(ePoint);
   }
+}
+
+
+template<typename T, typename Tint, typename Tint>
+std::vector<MyVector<Tint>> GetListIntegralPoint(MyMatrix<T> const& FAC, MyMatrix<T> const& EXT)
+{
+  std::vector<MyVector<Tint>> ListPoint;
+  auto f_insert=[&](const MyVector<Tint>& ePoint) -> void {
+    ListPoint.push_back(ePoint);
+  };
+  Kernel_GetListIntegralPoint(FAC, EXT, f_insert);
   return ListPoint;
 }
+
+
+
+
+template<typename T, typename Tint, typename Finsert>
+void Kernel_GetListIntegralPoint_LP(MyMatrix<T> const& FAC, Finsert f_insert)
+{
+  // Getting the bounds with the coordinates i<pos1 being fixed with values
+  // set by eVert. The values of the coordinate pos2 is then computed.
+  auto get_bound=[&](const MyVector<T>& eVert, const size_t& pos1, const size_t& pos2) -> std::pair<Tint,Tint> {
+  };
+  while(true) {
+  }
+}
+
+
+
+
+
 
 
 
