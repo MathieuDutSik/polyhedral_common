61ff0d5fabd8f9a47f186cb78d15ae1abab983bf
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-09-19 14:36:43 +0200

    Insert more code.

--- src_vinberg/VIN_ComputeDecomposition.cpp
+++ src_vinberg/VIN_ComputeDecomposition.cpp
@@ -36,15 +36,39 @@ int main(int argc, char* argv[])
     //
     int TheDim = G.rows();
     for (int i=1; i<TheDim-1; i++) {
-      using Tent = std::tuple<size_t,MyMatrix<Tint>,MyMatrix<Tint>,MyMatrix<Tint>>;
+      struct Tent {
+        MyMatrix<Tint> M;
+        MyMatrix<Tint> Spann;
+        MyMatrix<Tint> Qmat;
+        WeightMatrix<true,std::vector<Tint>,Tidx_value>> WMat;
+      };
+      auto f_subset=[&](const size_t& n_row, const size_t& len) -> Face {
+        Face subset(n_row);
+        for (size_t i=0; i<len; i++)
+          subset[i] = 1;
+        return subset;
+      };
       auto f_stab=[&](const Tent& eEnt) -> Tgroup {
+        Tgroup GRP1 = GetStabilizerWeightMatrix_Kernel<std::vector<Tint>,Tgr,Tidx,Tidx_value>(eEnt.WMat);
+        MyMatrix<Tint> Concat = ConcatenateMatrix(eEnt.M, eEnt.Spann);
+        Tgroup GRP2 = LinPolytopeIntegral_Stabilizer_Method8(Concat, GRP1);
+        Face subset = f_subset(Concat.rows(), eEnt.M.rows());
+        return ReducedGroupAction(GRP2, subset);
       };
       auto f_equiv=[&](const Tent& eEnt, const Tent& fEnt) -> std::option<MyMatrix<Tint>> {
-        if (std::get<0>(eEnt) == std::get<0>(fEnt))
+        MyMatrix<Tint> eConcat = ConcatenateMatrix(eEnt.M, eEnt.Spann);
+        MyMatrix<Tint> fConcat = ConcatenateMatrix(fEnt.M, fEnt.Spann);
+        std::vector<Tidx> eCanonicReord = GetGroupCanonicalizationVector_Kernel<T,Tgr,Tidx,Tidx_value>(eEnt.WMat).first;
+        std::vector<Tidx> fCanonicReord = GetGroupCanonicalizationVector_Kernel<T,Tgr,Tidx,Tidx_value>(fEnt.WMat).first;
+        // Computing the isomorphism
+        using Tfield = typename overlying_field<Tint>::field_type;
+        std::optional<std::pair<std::vector<Tidx>,MyMatrix<Tfield>>> IsoInfo = IsomorphismFromCanonicReord<T,Tfield,Tidx>(eConcat, fConcat, eCanonicReord, fCanonicReord);
+        if (!IsoInfo)
           return {};
-
+        Telt ePerm(IsoInfo->first);
+        return LinPolytopeIntegral_Isomorphism_Method8(eConcat, fConcat, GRP1, ePerm);
       };
-      std::vector<Tent> NewListCand;
+      std::vector<std::pair<size_t,Tent>> NewListCand;
       auto f_ent=[&](const MyMatrix<Tint>& M) -> Tent {
         MyMatrix<Tint> P = M * G;
         MyMatrix<Tint> NSP = NullspaceIntMat(P);
@@ -53,17 +77,26 @@ int main(int argc, char* argv[])
         MyMatrix<Tint> SHV = ExtractInvariantVectorFamilyZbasis<T,Tint>(Gres_T);
         MyMatrix<Tint> Spann = SHV * NSP;
         MyMatrix<Tint> Concat = ConcatenateMatrix(M, Spann);
-        size_t inv = 0;
         MyMatrix<Tint> Qmat = GetQmatrix(Concat);
-        return {inv, M, Spann, Qmat},
+        Face subset = f_subset(Concat.rows(), M.rows());
+        std::vector<MyMatrix<Tint>> ListMat{Qmat, G};
+        WeightMatrix<true,std::vector<Tint>,Tidx_value> WMat = GetWeightMatrix_ListMat_Subset<Tint,Tidx,Tidx_value>(EXT1, ListMat, subset);
+        WMat.ReorderingSetWeight();
+        return {M, Spann, Qmat, WMat},
+      };
+      auto f_inv=[&](const Tent& eEnt) -> size_t {
+        return std::hash<WeightMatrix<true, std::vector<Tint>, Tidx_value>>()(WMat);
       };
       auto f_insert=[&](const Tent& fEnt) -> void {
-        for (auto & eEnt : NewListCand) {
-          std::option<MyMatrix<Tint>> eEquiv = f_equiv(eEnt, fEnt);
-          if (eEquiv)
-            return;
+        size_t e_inv = f_inv(fEnt);
+        for (auto & eP : NewListCand) {
+          if (eP.first == e_inv) {
+            std::option<MyMatrix<Tint>> eEquiv = f_equiv(eP.second, fEnt);
+            if (eEquiv)
+              return;
+          }
         }
-        NewListCand.push_back(fEnt);
+        NewListCand.push_back({e_inv,fEnt});
       };
       for (auto & eDomain : ListListDomain[i-1]) {
         Tent eEnt = f_ent(eDomain);
@@ -82,15 +115,6 @@ int main(int argc, char* argv[])
         NewListDomain.push_back(std::get<1>(eEnt));
       ListListDomain.push_back(NewListDomain);
       std::cerr << "i=" << i << " |NewListDomain|=" << NewListDomain.size() << "\n";
-    }
-
-    //
-    if (argc == 2) {
-      Print_DataReflectionGroup(data, std::cerr);
-    } else {
-      std::string FileO = argv[2];
-      std::ofstream os(FileO);
-      Print_DataReflectionGroup(data, os);
     }
   }
   catch (TerminalException const& e) {
