c7c08d3dc44e3faf52bd61175bba1ef9a3d662bd
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-10-08 13:48:38 +0200

    Move file and insert.

---
+++ src_poly/WeightMatrixLimited.h
@@ -0,0 +1,195 @@
+#ifndef INCLUDE_WEIGHT_MATRIX_LIMITED_H
+#define INCLUDE_WEIGHT_MATRIX_LIMITED_H
+
+
+
+
+
+
+template<bool is_symmetric_impl, typename T_impl>
+struct WeightMatrixLimited {
+public:
+  static const bool is_symmetric = is_symmetric_impl;
+  using T = T_impl;
+  template<typename F1, typename F2>
+  WeightMatrix(size_t const& _nbRow, F1 f1, F2 f2, size_t max_offdiag) : nbRow(_nbRow)
+  {
+    // Computing the diagional values
+    list_diag_idx.resize(nbRow);
+    std::unordered_map<T, size_t> ValueMap;
+    size_t idxWeight=0;
+    for (size_t iRow=0; iRow<nbRow; iRow++) {
+      f1(iRow);
+      T val = f2(iRow);
+      size_t & idx = ValueMap[val];
+      if (idx == 0) {
+        idxWeight++;
+        idx = idxWeight;
+        list_diag_weight.push_back(val);
+      }
+      size_t pos_val = idx - 1;
+      list_diag_idx[iRow] = pos_val;
+    }
+    size_t n_weight = ValueMap.size();
+    // Coding the reordering of the list_diag_weight
+
+    // Computing the shift and sizes
+    list_diag_sizes.resize(n_weight);
+    list_diag_shift.resize(n_weight+1);
+    for (size_t i=0; i<n_weight; i++)
+      list_sizes[i] = 0;
+    for (size_t iRow=0; iRow<nbRow; iRow++) {
+      size_t iWeight = list_diag_idx[iRow];
+      list_diag_sizes[iWeight]++;
+    }
+    list_diag_shift[0] = 0;
+    for (size_t i=0; i<n_weight; i++)
+      list_diag_shift[i+1] = list_diag_shift[i] + list_diag_sizes[i];
+    std::vector<size_t> list_diag_pos = list_diag_shift;
+    list_diag_element.resize(nbRow);
+    for (size_t iRow=0; iRow<nbRow; iRow++) {
+      size_t iWeight = list_diag_idx[iRow];
+      size_t pos = list_diag_shift[iWeight];
+      list_diag_element[pos] = iRow;
+      list_diag_shift[iWeight] = pos + 1;
+    }
+    // Computing the set of off diagonal entries that we are interested in.
+    using Tpair = std::pair<size_t, size_t>;
+    std::map<size_t,std::vector<Tpair>> map_pair;
+    for (size_t i_weight=0; i_weight<n_weight; i_weight++) {
+      size_t start=0;
+      if (constexpr is_symmetric)
+        start = i_weight;
+      size_t siz1 = list_diag_sizes[i_weight];
+      for (size_t j_weight=0; j_weight<n_weight; j_weight++) {
+        if (i_weight == j_weight) {
+          if (constexpr is_symmetric) {
+            size_t n_poss = siz1 * (siz1 - 1) / 2;
+            map_pair[n_poss].push_back({i_weight, i_weight});
+          } else {
+            size_t n_poss = siz1 * (siz1 - 1);
+            map_pair[n_poss].push_back({i_weight, i_weight});
+          }
+        } else {
+          size_t siz2 = list_sizes[j_weight];
+          size_t n_poss = siz1 * siz2;
+          map_pair[n_poss].push_back({i_weight, j_weight});
+        }
+      }
+    }
+    std::vector<Tpair> list_selected;
+    auto iter = map_pair.cbegin();
+    size_t sel_size = 0;
+    while(true) {
+      if (iter == map_pair.cend())
+        break;
+      const std::vector<Tpair> & e_pair = iter->second;
+      size_t tot_siz = iter->first * (e_pair.size());
+      if (sel_size + tot_siz > max_offdiag)
+        break;
+      sel_size += tot_siz;
+      list_selected.insert(list_selected.end(), e_pair.begin(), e_pair.end());
+      iter++;
+    }
+    // Computing the arrays themselves
+    list_offdiag_idx.resize(sel_size);
+    ValueMap.clear();
+    idxWeight=0;
+    size_t pos = 0;
+    auto insertval=[&](size_t const& jRow) -> void {
+      T val = f2(jRow);
+      size_t & idx = ValueMap[val];
+      if (idx == 0) {
+        idxWeight++;
+        idx = idxWeight;
+        list_offdiag_weight.push_back(val);
+      }
+      size_t pos_val = idx - 1;
+      list_offdiag_idx[pos] = pos_val;
+      pos++;
+    };
+    for (auto& e_ent : list_selected) {
+      size_t i_wei = e_ent.first;
+      size_t j_wei = e_ent.second;
+      if (i_wei == j_wei) {
+        size_t siz = list_sizes[i_wei];
+        size_t shift = list_shift[i_wei];
+        if (constexpr is_symmetric) {
+          for (size_t i=0; i<siz; i++) {
+            size_t ipos = shift + i;
+            size_t iRow = list_diag_element[ipos];
+            f1(iRow);
+            for (size_t j=i+1; j<siz; j++) {
+              size_t jpos = shift + j;
+              size_t jRow = list_diag_element[jpos];
+              insertval(jRow);
+            }
+          }
+        } else {
+          for (size_t i=0; i<siz; i++) {
+            size_t ipos = shift + i;
+            size_t iRow = list_diag_element[ipos];
+            f1(iRow);
+            for (size_t j=0; j<siz; j++) {
+              if (i != j) {
+                size_t jpos = shift + j;
+                size_t jRow = list_diag_element[jpos];
+                insertval(jRow);
+              }
+            }
+          }
+        }
+      } else {
+        size_t i_siz = list_sizes[i_wei];
+        size_t i_shift = list_shift[i_wei];
+        size_t j_siz = list_sizes[j_wei];
+        size_t j_shift = list_shift[j_wei];
+        for (size_t i=0; i<i_siz; i++) {
+          size_t ipos = i_shift + i;
+          size_t iRow = list_diag_element[ipos];
+          f1(iRow);
+          for (size_t j=0; j<j_siz; j++) {
+            size_t jpos = j_shift + j;
+            size_t jRow = list_diag_element[jpos];
+            insertval(jRow);
+          }
+        }
+      }
+    }
+    // Redordering of the ValueMap.
+  }
+
+private:
+  size_t nbRow;
+  std::vector<T> list_diag_weight;
+  //
+  std::vector<size_t> list_idx_diag;
+  std::vector<size_t> list_diag_sizes, list_diag_shift;
+  std::vector<size_t> list_diag_elements;
+  //
+  std::vector<T> list_offdiag_weight;
+  std::vector<size_t> list_offdiag_idx;
+  std::vector<size_t> list_offdiag_sizes;
+  std::vector<size_t> list_offdiag_shifts;
+  std::vector<size_t> list_offdiag_elements;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
