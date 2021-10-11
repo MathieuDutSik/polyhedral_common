e1609eb41d53ddd7c40640933448ce3e2891755f
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-10-11 18:12:46 +0200

    Some simplification.

--- src_poly/WeightMatrixLimited.h
+++ src_poly/WeightMatrixLimited.h
@@ -11,12 +11,58 @@ struct WeightMatrixLimited {
 public:
   static const bool is_symmetric = is_symmetric_impl;
   using T = T_impl;
+private:
+  void reorder_entries(std::vector<T> & list_weight, std::vector<size_t> & list_idx)
+  {
+    std::map<T, size_t> map_value;
+    size_t n_ent = list_weight.size();
+    for (size_t i=0; i<n_ent; i++)
+      map_value[list_diag_weight_tmp[i]] = i;
+    std::vector<size_t> g(n_ent);
+    size_t idx=0;
+    for (auto & kv : map_value) {
+      g[kv.second] = idx;
+      idx++;
+    }
+    size_t len = list_idx.size();
+    for (size_t i=0; i<len; i++)
+      list_idx[i] = g[list_idx[i]];
+    std::vector<T> list_weight_new(n_ent);
+    for (size_t i=0; i<n_ent; i++)
+      list_weight_new[g[i]] = list_weight[i];
+    list_weight = std::move(list_weight_new);
+  }
+  void compute_shift_size(std::vector<size_t> & list_sizes, std::vector<size_t> & list_shift, std::vector<size_t> & list_element,
+                          const std::vector<size_t> & list_idx, const std::vector<T> & list_weight)
+  {
+    size_t n_weight = list_weight.size();
+    size_t len = list_idx.size();
+    list_sizes.resize(n_weight);
+    list_shift.resize(n_weight+1);
+    for (size_t i=0; i<n_weight; i++)
+      list_sizes[i] = 0;
+    for (size_t iRow=0; iRow<nbRow; iRow++) {
+      size_t iWeight = list_diag_idx[iRow];
+      list_diag_sizes[iWeight]++;
+    }
+    list_shift[0] = 0;
+    for (size_t i=0; i<n_weight; i++)
+      list_shift[i+1] = list_shift[i] + list_sizes[i];
+    std::vector<size_t> list_pos = list_shift;
+    list_element.resize(len);
+    for (size_t i=0; i<len; i++) {
+      size_t iWeight = list_idx[i];
+      size_t pos = list_pos[iWeight];
+      list_element[pos] = iRow;
+      list_pos[iWeight] = pos + 1;
+    }
+  }
+public:
   template<typename F1, typename F2>
   WeightMatrix(size_t const& _nbRow, F1 f1, F2 f2, size_t max_offdiag) : nbRow(_nbRow)
   {
     // Computing the diagional values
     list_diag_idx.resize(nbRow);
-    std::vector<T> list_diag_weight_tmp;
     std::unordered_map<T, size_t> unordmap_value;
     size_t idxWeight=0;
     for (size_t iRow=0; iRow<nbRow; iRow++) {
@@ -26,48 +72,16 @@ public:
       if (idx == 0) {
         idxWeight++;
         idx = idxWeight;
-        list_diag_weight_tmp.push_back(val);
+        list_diag_weight.push_back(val);
       }
       size_t pos_val = idx - 1;
       list_diag_idx[iRow] = pos_val;
     }
     size_t n_weight = unordmap_value.size();
     // Coding the reordering of the list_diag_weight
-    std::map<T, size_t> map_value;
-    size_t n_ent = list_diag_weight_tmp.size();
-    for (size_t i=0; i<n_ent; i++)
-      map_value[list_diag_weight_tmp[i]] = i;
-    std::vector<size_t> g(n_ent);
-    size_t idx=0;
-    for (auto & kv : map_value) {
-      g[kv.second] = idx;
-      idx++;
-    }
-    for (size_t iRow=0; iRow<nbRow; iRow++)
-      list_diag_idx[iRow] = g[list_diag_idx[iRow]];
-    list_diag_weight.resize(n_ent);
-    for (size_t i=0; i<n_ent; i++)
-      list_diag_weight[g[i]] = list_diag_weight_tmp[i];
+    reorder_entries(list_diag_weight, list_diag_idx);
     // Computing the shift and sizes
-    list_diag_sizes.resize(n_weight);
-    list_diag_shift.resize(n_weight+1);
-    for (size_t i=0; i<n_weight; i++)
-      list_diag_sizes[i] = 0;
-    for (size_t iRow=0; iRow<nbRow; iRow++) {
-      size_t iWeight = list_diag_idx[iRow];
-      list_diag_sizes[iWeight]++;
-    }
-    list_diag_shift[0] = 0;
-    for (size_t i=0; i<n_weight; i++)
-      list_diag_shift[i+1] = list_diag_shift[i] + list_diag_sizes[i];
-    std::vector<size_t> list_diag_pos = list_diag_shift;
-    list_diag_element.resize(nbRow);
-    for (size_t iRow=0; iRow<nbRow; iRow++) {
-      size_t iWeight = list_diag_idx[iRow];
-      size_t pos = list_diag_shift[iWeight];
-      list_diag_element[pos] = iRow;
-      list_diag_shift[iWeight] = pos + 1;
-    }
+    compute_shift_size(list_diag_sizes, list_diag_shift, list_diag_element, list_diag_idx, list_diag_weight);
     // Computing the set of off diagonal entries that we are interested in.
     using Tpair = std::pair<size_t, size_t>;
     std::map<size_t,std::vector<Tpair>> map_pair;
@@ -107,7 +121,6 @@ public:
       iter++;
     }
     // Computing the arrays themselves
-    std::vector<T> list_offdiag_weight_tmp;
     list_offdiag_idx.resize(sel_size);
     unordmap_value.clear();
     idxWeight=0;
@@ -118,7 +131,7 @@ public:
       if (idx == 0) {
         idxWeight++;
         idx = idxWeight;
-        list_offdiag_weight_tmp.push_back(val);
+        list_offdiag_weight.push_back(val);
       }
       size_t pos_val = idx - 1;
       list_offdiag_idx[pos] = pos_val;
@@ -173,49 +186,22 @@ public:
       }
     }
     // Reordering of the unordmap_value.
-    map_value.clear();
-    n_ent = list_offdiag_weight_tmp.size();
-    for (size_t i=0; i<n_ent; i++)
-      map_value[list_offdiag_weight_tmp[i]] = i;
-    g.resize(n_ent);
-    idx=0;
-    for (auto & kv : map_value) {
-      g[kv.second] = idx;
-      idx++;
-    }
-    for (size_t iRow=0; iRow<nbRow; iRow++)
-      list_offdiag_idx[iRow] = g[list_offdiag_idx[iRow]];
-    list_offdiag_weight.resize(n_ent);
-    for (size_t i=0; i<n_ent; i++)
-      list_offdiag_weight[g[i]] = list_offdiag_weight_tmp[i];
+    reorder_entries(list_offdiag_weight, list_offdiag_idx);
     // Now computing shifting information
-    list_offdiag_sizes.resize(n_weight);
-    list_offdiag_shift.resize(n_weight+1);
-    for (size_t i=0; i<n_weight; i++)
-      list_offdiag_sizes[i] = 0;
-    for (size_t iRow=0; iRow<nbRow; iRow++) {
-      size_t iWeight = list_diag_idx[iRow];
-      list_diag_sizes[iWeight]++;
-    }
-    list_diag_shift[0] = 0;
-    for (size_t i=0; i<n_weight; i++)
-      list_diag_shift[i+1] = list_diag_shift[i] + list_diag_sizes[i];
-    std::vector<size_t> list_diag_pos = list_diag_shift;
-    list_diag_element.resize(nbRow);
-    for (size_t iRow=0; iRow<nbRow; iRow++) {
-      size_t iWeight = list_diag_idx[iRow];
-      size_t pos = list_diag_shift[iWeight];
-      list_diag_element[pos] = iRow;
-      list_diag_shift[iWeight] = pos + 1;
-    }
+    compute_shift_size(list_offdiag_sizes, list_offdiag_shift, list_offdiag_element, list_offdiag_idx, list_offdiag_weight);
   }
-
+  template<typename Tgroup>
+  WeightMatrix(const Tgroup& GRP) : nbRow(GRP.n_act())
+  {
+    
+  }
 private:
   size_t nbRow;
-  std::vector<T> list_diag_weight;
   //
-  std::vector<size_t> list_idx_diag;
-  std::vector<size_t> list_diag_sizes, list_diag_shift;
+  std::vector<T> list_diag_weight;
+  std::vector<size_t> list_diag_idx;
+  std::vector<size_t> list_diag_sizes;
+  std::vector<size_t> list_diag_shift;
   std::vector<size_t> list_diag_elements;
   //
   std::vector<T> list_offdiag_weight;
