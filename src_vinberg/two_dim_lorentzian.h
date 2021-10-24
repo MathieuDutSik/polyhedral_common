8fca329582072cbae6851609e7f58a0ee7ca28df
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-10-24 18:38:01 +0200

    Initial brain dump.

---
+++ src_vinberg/two_dim_lorentzian.h
@@ -0,0 +1,147 @@
+#ifndef INCLUDE_TWO_DIM_LORENTZIAN_H
+#define INCLUDE_TWO_DIM_LORENTZIAN_H
+
+/*
+  We implement here algorithms from Section 8 in
+  "AN ALTERNATIVE TO VINBERG’S ALGORITHM"
+
+  We work with a form of signature (n,1) (also in Allcock paper.
+  In this article:
+  ---"timelike" vectors are vector of negative norm
+  (which are contained in the cone).
+  ---"lightlike" vectors have zero norm
+
+  R k is the boundary of the half plane.
+
+  S is a set of vectors of posiive norm.
+  What means "l must not lie in the interior of the
+  convex hull of the norm M hyperbola in S"
+
+ */
+
+template<typename T, typename Tint>
+T eval(const MyMatrix<T>& G, const MyVector<Tint>& v1, const MyVector<Tint>& v2)
+{
+  return v1(0) * v2(0) * G(0,0) + G(0,1) * (v1(0) * v2(1) + v1(1) * v2(0)) + v1(1) * v2(1) * G(1,1);
+}
+
+
+
+template<typename T, typename Tint>
+std::pair<MyVector<Tint>, MyVector<Tint>> Isotropic(const MyMatrix<T>& G, const T&M, const MyVector<Tint>& r, const MyVector<Tint>& l)
+{
+  MyVector<Tint> m = r + l;
+  T norm_mm = eval(G, m, m);
+  T scal_ml = eval(G, m, l);
+  if (norm_mm <= M || scal_ml < 0)
+    return Isotropic(G, M, r, m);
+  if (eval(G, l, l) >= 0 && eval(G, r, l) > 0)
+    return {l, -r};
+  return Isotropic(G, M, m, r);
+}
+
+
+template<typename T, typename Tint>
+Tint LowerSquareRoot(const T& val)
+{
+  auto is_lower=[&](const Tint& x) -> bool {
+    T x_T = x;
+    return val - x_T * x_T >= 0;
+  };
+  double val_d = UniversalScalarConversion<double,T>(val);
+  double val_sqrt = floor(sqrt(val_d));
+  Tint x_guess = UniversalScalarConversion<Tint,double>(val_sqrt);
+  while(true) {
+    bool test1 = is_lower(x_guess);
+    bool test2 = is_lower(x_guess+1);
+    if (test1 && !test2) {
+      return x_guess;
+    } else {
+      if (test2) {
+        x_guess++;
+      } else {
+        if (!test1)
+          x_guess--;
+      }
+  }
+}
+
+
+template<typename T, typename Tint>
+MyVector<Tint> Canonical(const MyMatrix<T>& G, const T&M, const MyVector<Tint>& r, const MyVector<Tint>& l)
+{
+  T scal1 = eval(G, r, l);
+  T scal2 = eval(G, r, r);
+  T scal3 = eval(G, l, l);
+  T e_ent = scal1 * scal1 - scal2 * (scal3 - M);
+  if (e_ent < 0) {
+    std::cerr << "We cannot compute square root of a negative number\n";
+    throw TerminalException{1};
+  }
+  Tint low_sqrt_tint =  LowerSquareRoot(e_ent);
+  T quot = (-scal1 + low_sqrt_tint) / scal2;
+  Tint K = UniversalFloorScalarConversion<Tint,T>(quot);
+  MyVector<Tint> l_new = l + K * r;
+  return l_new;
+}
+
+
+template<typename T, typename Tint>
+std::optional<std::pair<MyVector<Tint>, MyVector<Tint>>> Shorter(const MyMatrix<T>& G, MyVector<Tint> r, MyVector<Tint> l)
+{
+  auto get_char_mat=[&](const MyVector<Tint>& v1, const MyVector<Tint>& v2) -> std::vector<T> {
+    return {eval(G, v1, v1), eval(G, v1, v2), eval(G, v2, v2)};
+  };
+  T M = eval(G, r, r);
+  std::vector<T> char_ent1 = get_char_mat(r, l);
+  T scal_rr;
+  while(true) {
+    // Step 2
+    scal_rr = eval(G, r, r);
+    std::pair<MyVector<Tint>, MyVector<Tint>> e_pair = Isotropic(G, scal_rr, r, l);
+    r = e_pair.first;
+    l = e_pair.second;
+    // Step 3
+    scal_rr = eval(G, r, r);
+    l = Canonicalize(scal_rr, r, l);
+    // Step 4
+    if (scal_rr < M)
+      return {r, l};
+    // Step 5
+    std::vector<T> char_ent2 = get_char_mat(r, l);
+    if (char_ent1 == char_ent2)
+      return {};
+  }
+}
+
+
+
+template<typename T, typename Tint>
+std::optional<std::pair<MyVector<Tint>, MyVector<Tint>>> NotPromised(const MyMatrix<T>& G, const T& M, MyVector<Tint> r, MyVector<Tint> l)
+{
+  T scal_rr = eval(G, r, r);
+  if (scal_rr <= M) {
+    std::pair<MyVector<Tint>, MyVector<Tint>> e_pair = Isotropic(G, M, r, l);
+    r = e_pair.first;
+    l = e_pair.second;
+    l = Canonical(G, M, r, l);
+    return {r, l};
+  }
+  while(true) {
+    std::optional<std::pair<MyVector<Tint>, MyVector<Tint>>> opt_pair = Shorter(G, r, l);
+    if (!opt_pair)
+      return {};
+    r = opt_pair->first;
+    l = opt_pair->second;
+    if (eval(G, r, r) <= M) {
+      l = Canonical(G, M, r, l);
+      return {r, l};
+    }
+  }
+}
+
+
+
+
+
+#endif
