934ffb98f1093fc78c545f64d163a75824a34286
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-02-10 10:39:04 +0100

    Small correction.

--- src_poly/POLY_c_cddlib_mpq.h
+++ src_poly/POLY_c_cddlib_mpq.h
@@ -6,11 +6,6 @@
 #include "MatrixTypes.h"
 
 namespace cbased_cdd {
-
   vectface DualDescription_incd_mpq_class(MyMatrix<mpq_class> const& TheEXT);
-
-#ifdef INCLUDE_NUMBER_THEORY_BOOST_GMP_INT
   vectface DualDescription_incd_boost_mpq_rational(MyMatrix<boost::multiprecision::mpq_rational> const& TheEXT);
-#endif
-
 }
