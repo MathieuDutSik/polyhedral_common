e50314d5952e6a70dd5d0f161190c13f856af665
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-02-15 08:28:46 +0100

    Several corrections for better testing.

--- src_poly/TEST_PermMatrTest_PolyStabilizer.cpp
+++ src_poly/TEST_PermMatrTest_PolyStabilizer.cpp
@@ -10,26 +10,63 @@
 int main(int argc, char *argv[])
 {
   try {
-    if (argc != 3) {
+    if (argc != 4) {
       std::cerr << "Number of argument is = " << argc << "\n";
       std::cerr << "This program is used as\n";
-      std::cerr << "GRP_PermMatrTest_Automorphism [MatrFile] [FaceFile]\n";
+      std::cerr << "GRP_PermMatrTest_PolyAutomorphism single [MatrFile] [FaceFile]\n";
+      std::cerr << "GRP_PermMatrTest_PolyAutomorphism random [MatrFile] [k]\n";
+      std::cerr << "GRP_PermMatrTest_PolyAutomorphism multiple [ListMatrFile] [k]\n";
       return -1;
     }
     using T = mpq_class;
     //
     std::cerr << "GRP_ComputeAut_ListMat_Subset_EXT : Reading input\n";
     //
-    std::string MatrFile = argv[1];
-    std::string FaceFile = argv[2];
-
-    MyMatrix<T> M = ReadMatrixFile<T>(MatrFile);
-    Face f = ReadFaceFile(FaceFile);
-
-    TestPolytopeFace(M, f);
-
+    auto process=[&]() -> void {
+      std::string option = argv[1];
+      if (option == "single") {
+        std::string MatrFile = argv[2];
+        MyMatrix<T> M = ReadMatrixFile<T>(MatrFile);
+        std::string FaceFile = argv[3];
+        Face f = ReadFaceFile(FaceFile);
+        TestPolytopeFace(M, f);
+        return;
+      }
+      if (option == "random") {
+        std::string MatrFile = argv[2];
+        MyMatrix<T> M = ReadMatrixFile<T>(MatrFile);
+        int k;
+        sscanf(argv[3], "%d", &k);
+        for (int iter=0; iter<100; iter++) {
+          Face f = RandomKFace(M.rows(), k);
+          TestPolytopeFace(M, f);
+        }
+        return;
+      }
+      if (option == "multiple") {
+        std::string ListMatrFile = argv[2];
+        std::ifstream is(ListMatrFile);
+        int k;
+        sscanf(argv[3], "%d", &k);
+        int n_file;
+        is >> n_file;
+        for (int i_file=0; i_file<n_file; i_file++) {
+          MyMatrix<T> M = ReadMatrix<T>(is);
+          for (int iter=0; iter<100; iter++) {
+            Face f = RandomKFace(M.rows(), k);
+            TestPolytopeFace(M, f);
+          }
+        }
+        return;
+      }
+      std::cerr << "Allowed options are single, random, multiple\n";
+      throw TerminalException{1};
+    };
+    process();
+    std::cerr << "Normal termination of the program\n";
   }
   catch (TerminalException const& e) {
+    std::cerr << "Something went wrong\n";
     exit(e.eVal);
   }
 }
