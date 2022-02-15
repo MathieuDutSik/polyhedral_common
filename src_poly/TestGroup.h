6d8fb64316ad3574ca32a0037ce35feba3a82bd7
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-02-15 11:16:22 +0100

    Participation to the bug resolution.

--- src_poly/TestGroup.h
+++ src_poly/TestGroup.h
@@ -16,43 +16,46 @@ void TestPolytopeFace(MyMatrix<T> const& M, Face const& f)
   using Telt1   = permutalib::SingleSidedPerm<Tidx>;
   using Tgroup1 = permutalib::Group<Telt1,Tint>;
   //
+  std::cerr << "---------------------------------------- TestPolytopeFace -------------------------------------\n";
   int n_vert = M.rows();
   MyMatrix<T> id_matr = IdentityMat<T>(M.cols());
   std::cerr << "GRP_ComputeAut_ListMat_Subset_EXT : Reading input\n";
   Tgroup1 GRP1 = LinPolytope_Automorphism<T,use_scheme1,Tgroup1>(M);
-  std::cerr << "We have GRP1 n_vert=" << n_vert << "\n";
+  std::cerr << "We have |GRP1|=" << GRP1.size() << "  n_vert=" << n_vert << "\n";
   Face fo = OrbitUnion(GRP1, f);
   std::cerr << "|f|=" << f.size() << " / " << f.count() << "    |fo|=" << fo.size() << " / " << fo.count() << "\n";
   std::vector<int> o_v;
   for (int i=0; i<n_vert; i++)
     if (fo[i] == 1)
       o_v.push_back(i);
-  std::cerr << "o_v =";
-  for (auto & val : o_v)
-    std::cerr << " " << val;
-  std::cerr << "\n";
   std::vector<int> o_v_rev(n_vert,-1);
   for (size_t pos=0; pos<o_v.size(); pos++) {
     int val = o_v[pos];
     //    std::cerr << "pos=" << pos << " val=" << val << "\n";
     o_v_rev[val] = pos;
   }
+  /*
+  std::cerr << "o_v =";
+  for (auto & val : o_v)
+    std::cerr << " " << val;
+  std::cerr << "\n";
   std::cerr << "o_v_rev =";
   for (auto & val : o_v_rev)
     std::cerr << " " << val;
   std::cerr << "\n";
   std::cerr << "We have fo, o_v_rev\n";
+  */
   std::unordered_map<MyVector<T>, int> map;
   for (int i_vert=0; i_vert<n_vert; i_vert++) {
     MyVector<T> V = GetMatrixRow(M, i_vert);
     map[V] = i_vert;
   }
-  std::cerr << "We have map\n";
+  //  std::cerr << "We have map\n";
   //
   auto get_group_A=[&](std::vector<MyMatrix<T>> const LMat, std::vector<Telt1> const& LElt, Face const& f) -> Tgroup1 {
-    std::cerr << "Before computation of LStabMatrGen\n";
+    //    std::cerr << "Before computation of LStabMatrGen\n";
     std::vector<MyMatrix<T>> LStabMatrGen = permutalib::StabilizerMatrixPermSubset<Telt1,MyMatrix<T>,mpz_class>(LMat, LElt, id_matr, f);
-    std::cerr << " After computation of LStabMatrGen\n";
+    //    std::cerr << " After computation of LStabMatrGen\n";
     std::vector<Telt1> LStabPermGen;
     for (auto & eGen : LStabMatrGen) {
       std::vector<Tidx> eList(n_vert);
@@ -91,11 +94,11 @@ void TestPolytopeFace(MyMatrix<T> const& M, Face const& f)
   }
 
   Tgroup1 stab1 = get_group_A(ListMatrGens, ListPermGens_A, f);
-  std::cerr << "We have stab1\n";
+  std::cerr << "We have stab1 |stab1|=" << stab1.size() << "\n";
   Tgroup1 stab2 = get_group_A(ListMatrGens, ListPermGens_B, f_B);
-  std::cerr << "We have stab2\n";
+  std::cerr << "We have stab2 |stab2|=" << stab2.size() << "\n";
   Tgroup1 stab3 = GRP1.Stabilizer_OnSets(f);
-  std::cerr << "We have stab3\n";
+  std::cerr << "We have stab3 |stab3|=" << stab3.size() << "\n";
   if (stab1 != stab2 || stab1 != stab3) {
     std::cerr << "The groups are not equal. Please debug\n";
     throw TerminalException{1};
