6be24e89ff7e9c0fe50403e0a4f4b19719eb642f
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-03-23 12:06:44 +0100

    More correction.

--- src_dualdesc/Databank.h
+++ src_dualdesc/Databank.h
@@ -1,36 +1,31 @@
-#ifndef INCLUDE_DATABANK_H
-#define INCLUDE_DATABANK_H
+#ifndef SRC_DUALDESC_DATABANK_H_
+#define SRC_DUALDESC_DATABANK_H_
 
-#include <boost/asio.hpp>
 #include "basic_datafile.h"
+#include <boost/asio.hpp>
+#include <string>
+#include <utility>
+#include <unordered_map>
 
-
-template<typename Tgroup_impl>
-struct PairStore {
+template <typename Tgroup_impl> struct PairStore {
   using Tgroup = Tgroup_impl;
   Tgroup GRP;
   vectface ListFace;
 };
 
-
-
 namespace boost::serialization {
 
-  template<class Archive, typename Tgroup>
-  inline void serialize(Archive & ar, PairStore<Tgroup> & pair, const unsigned int version)
-  {
-    ar & make_nvp("GRP", pair.GRP);
-    ar & make_nvp("ListFace", pair.ListFace);
-  }
-
+template <class Archive, typename Tgroup>
+inline void serialize(Archive &ar, PairStore<Tgroup> &pair,
+                      const unsigned int version) {
+  ar &make_nvp("GRP", pair.GRP);
+  ar &make_nvp("ListFace", pair.ListFace);
 }
 
+} // namespace boost::serialization
 
-
-
-template<typename Tkey, typename Tval>
-std::pair<Tkey, Tval> Read_BankEntry(std::string const& Prefix)
-{
+template <typename Tkey, typename Tval>
+std::pair<Tkey, Tval> Read_BankEntry(std::string const &Prefix) {
   using T = typename Tkey::value_type;
   using Tgroup = typename Tval::Tgroup;
   std::string eFileEXT = Prefix + ".ext";
@@ -52,27 +47,26 @@ std::pair<Tkey, Tval> Read_BankEntry(std::string const& Prefix)
   //
   FileFace ff(eFileFF, n_row, n_orbit);
   vectface ListFace(n_row);
-  for (size_t i_orbit=0; i_orbit<n_orbit; i_orbit++) {
+  for (size_t i_orbit = 0; i_orbit < n_orbit; i_orbit++) {
     Face eFace = ff.getface(i_orbit);
     ListFace.push_back(eFace);
   }
-  std::cerr << " |EXT|=" << EXT.rows() << " |ListFace|=" << ListFace.size() << "\n";
+  std::cerr << " |EXT|=" << EXT.rows() << " |ListFace|=" << ListFace.size()
+            << "\n";
   Tval eVal{std::move(GRP), std::move(ListFace)};
   return {std::move(EXT), std::move(eVal)};
 }
 
-
-
-
-template<typename T, typename Tgroup>
-void Write_BankEntry(const std::string& Prefix, const MyMatrix<T>& EXT, const PairStore<Tgroup>& ePair)
-{
+template <typename T, typename Tgroup>
+void Write_BankEntry(const std::string &Prefix, const MyMatrix<T> &EXT,
+                     const PairStore<Tgroup> &ePair) {
   std::string eFileEXT = Prefix + ".ext";
   std::string eFileGRP = Prefix + ".grp";
   std::string eFileNB = Prefix + ".nb";
   std::string eFileFF = Prefix + ".ff";
   if (!FILE_IsFileMakeable(eFileEXT)) {
-    std::cerr << "Error in Write_BankEntry: File eFileEXT=" << eFileEXT << " is not makeable\n";
+    std::cerr << "Error in Write_BankEntry: File eFileEXT=" << eFileEXT
+              << " is not makeable\n";
     throw TerminalException{1};
   }
   //
@@ -88,17 +82,9 @@ void Write_BankEntry(const std::string& Prefix, const MyMatrix<T>& EXT, const Pa
   fn.setval(n_orbit);
   //
   FileFace ff(eFileFF, n_row);
-  for (size_t i_orbit=0; i_orbit<n_orbit; i_orbit++)
+  for (size_t i_orbit = 0; i_orbit < n_orbit; i_orbit++)
     ff.setface(i_orbit, ePair.ListFace[i_orbit]);
 }
-
-
-
-
-
-
-
-
 
 // It is better to use std::unordered_map for the List of entries:
 // This makes the check of equality rarer and instead uses the hash
@@ -106,35 +92,34 @@ void Write_BankEntry(const std::string& Prefix, const MyMatrix<T>& EXT, const Pa
 // Plus it is better because the tsl::sparse_map requires having the
 // copy operator and we want to avoid that for the vectface.
 
-
-template<typename Tkey, typename Tval>
-struct DataBank {
+template <typename Tkey, typename Tval> struct DataBank {
 private:
   std::unordered_map<Tkey, Tval> ListEnt;
   bool Saving;
   std::string SavingPrefix;
   Tval TrivElt;
+
 public:
-  DataBank(const bool& _Saving, const std::string& _SavingPrefix) : Saving(_Saving), SavingPrefix(_SavingPrefix)
-  {
+  DataBank(const bool &_Saving, const std::string &_SavingPrefix)
+      : Saving(_Saving), SavingPrefix(_SavingPrefix) {
     if (Saving) {
-      size_t iOrbit=0;
-      while(true) {
+      size_t iOrbit = 0;
+      while (true) {
         std::string Prefix = SavingPrefix + "DualDesc" + std::to_string(iOrbit);
         std::string eFileBank = Prefix + ".ext";
         if (!IsExistingFile(eFileBank))
           break;
         std::cerr << "Read iOrbit=" << iOrbit << " Prefix=" << Prefix << "\n";
-        std::pair<Tkey, Tval> PairKV = Read_BankEntry<Tkey,Tval>(Prefix);
-        ListEnt.emplace(std::make_pair<Tkey, Tval>(std::move(PairKV.first), std::move(PairKV.second)));
+        std::pair<Tkey, Tval> PairKV = Read_BankEntry<Tkey, Tval>(Prefix);
+        ListEnt.emplace(std::make_pair<Tkey, Tval>(std::move(PairKV.first),
+                                                   std::move(PairKV.second)));
         iOrbit++;
       }
     }
   }
-  void InsertEntry(Tkey && eKey, Tval && eVal)
-  {
-    // We have to face the situation that what we are trying to insert is already present
-    // This can happen because of badly aligned heuristics
+  void InsertEntry(Tkey &&eKey, Tval &&eVal) {
+    // We have to face the situation that what we are trying to insert is
+    // already present This can happen because of badly aligned heuristics
     if (ListEnt.count(eKey) > 0) {
       std::cerr << "Exiting because the key is already present\n";
       return;
@@ -145,27 +130,24 @@ public:
       std::cerr << "Insert entry to file Prefix=" << Prefix << "\n";
       Write_BankEntry(Prefix, eKey, eVal);
     }
-    ListEnt.emplace(std::make_pair<Tkey, Tval>(std::move(eKey), std::move(eVal)));
+    ListEnt.emplace(
+        std::make_pair<Tkey, Tval>(std::move(eKey), std::move(eVal)));
   }
-  const Tval& GetDualDesc(const Tkey& eKey) const
-  {
+  const Tval &GetDualDesc(const Tkey &eKey) const {
     std::cerr << "Passing by GetDualDesc |ListEnt|=" << ListEnt.size() << "\n";
-    typename std::unordered_map<Tkey, Tval>::const_iterator iter = ListEnt.find(eKey);
+    typename std::unordered_map<Tkey, Tval>::const_iterator iter =
+        ListEnt.find(eKey);
     if (iter == ListEnt.end())
-      return TrivElt; // If returning empty then it means nothing has been found.
+      return TrivElt; // If returning empty then it means nothing has been
+                      // found.
     return iter->second;
   }
 };
 
-
-
-
-template<typename T>
-T read_data(boost::asio::ip::tcp::socket & socket)
-{
+template <typename T> T read_data(boost::asio::ip::tcp::socket &socket) {
   boost::asio::streambuf buf;
-  boost::asio::read_until( socket, buf, "\n" );
-  std::string data = boost::asio::buffer_cast<const char*>(buf.data());
+  boost::asio::read_until(socket, buf, "\n");
+  std::string data = boost::asio::buffer_cast<const char *>(buf.data());
   T data_o;
   std::stringstream iss(data);
   boost::archive::text_iarchive ia(iss);
@@ -173,9 +155,8 @@ T read_data(boost::asio::ip::tcp::socket & socket)
   return data_o;
 }
 
-
-template<typename T>
-void send_data(boost::asio::ip::tcp::socket & socket, const T& data) {
+template <typename T>
+void send_data(boost::asio::ip::tcp::socket &socket, const T &data) {
   std::ostringstream archive_stream;
   boost::archive::text_oarchive archive(archive_stream);
   archive << data;
@@ -188,8 +169,9 @@ void send_data(boost::asio::ip::tcp::socket & socket, const T& data) {
   }
 }
 
-template<typename T>
-void send_data_atomic(const boost::asio::ip::tcp::endpoint& endpoint, const T& data) {
+template <typename T>
+void send_data_atomic(const boost::asio::ip::tcp::endpoint &endpoint,
+                      const T &data) {
   boost::asio::io_service io_service;
   boost::asio::ip::tcp::socket socket(io_service);
   socket.connect(endpoint);
@@ -197,54 +179,46 @@ void send_data_atomic(const boost::asio::ip::tcp::endpoint& endpoint, const T& d
   socket.close();
 }
 
-
-
-
-
-
-template<typename Tkey, typename Tval>
-struct TripleNKV {
+template <typename Tkey, typename Tval> struct TripleNKV {
   char nature;
   Tkey eKey;
   Tval eVal;
 };
 
-
 namespace boost::serialization {
 
-  template<class Archive, typename Tkey, typename Tval>
-  inline void serialize(Archive & ar, TripleNKV<Tkey,Tval> & triple, const unsigned int version)
-  {
-    ar & make_nvp("nature", triple.nature);
-    ar & make_nvp("eKey", triple.eKey);
-    ar & make_nvp("eVal", triple.eVal);
-  }
-
+template <class Archive, typename Tkey, typename Tval>
+inline void serialize(Archive &ar, TripleNKV<Tkey, Tval> &triple,
+                      const unsigned int version) {
+  ar &make_nvp("nature", triple.nature);
+  ar &make_nvp("eKey", triple.eKey);
+  ar &make_nvp("eVal", triple.eVal);
 }
 
+} // namespace boost::serialization
 
-
-
-template<typename Tkey, typename Tval>
-struct DataBankServer {
+template <typename Tkey, typename Tval> struct DataBankServer {
 private:
   std::unordered_map<Tkey, Tval> ListEnt;
   bool Saving;
   std::string SavingPrefix;
   short unsigned int port;
+
 public:
-DataBankServer(const bool& _Saving, const std::string& _SavingPrefix, const short unsigned int _port) : Saving(_Saving), SavingPrefix(_SavingPrefix), port(_port)
-  {
+  DataBankServer(const bool &_Saving, const std::string &_SavingPrefix,
+                 const short unsigned int _port)
+      : Saving(_Saving), SavingPrefix(_SavingPrefix), port(_port) {
     if (Saving) {
-      size_t iOrbit=0;
-      while(true) {
+      size_t iOrbit = 0;
+      while (true) {
         std::string Prefix = SavingPrefix + "DualDesc" + std::to_string(iOrbit);
         std::string eFileBank = Prefix + ".ext";
         if (!IsExistingFile(eFileBank))
           break;
         std::cerr << "Read iOrbit=" << iOrbit << " Prefix=" << Prefix << "\n";
-        std::pair<Tkey, Tval> PairKV = Read_BankEntry<Tkey,Tval>(Prefix);
-        ListEnt.emplace(std::make_pair<Tkey, Tval>(std::move(PairKV.first), std::move(PairKV.second)));
+        std::pair<Tkey, Tval> PairKV = Read_BankEntry<Tkey, Tval>(Prefix);
+        ListEnt.emplace(std::make_pair<Tkey, Tval>(std::move(PairKV.first),
+                                                   std::move(PairKV.second)));
         iOrbit++;
       }
     }
@@ -252,10 +226,10 @@ DataBankServer(const bool& _Saving, const std::string& _SavingPrefix, const shor
     boost::asio::io_service io_service;
     boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), port);
     boost::asio::ip::tcp::acceptor acceptor(io_service, endpoint);
-    size_t n_iter=0;
+    size_t n_iter = 0;
     while (true) {
       boost::asio::ip::tcp::socket socket = acceptor.accept();
-      TripleNKV<Tkey, Tval> eTriple = read_data<TripleNKV<Tkey,Tval>>(socket);
+      TripleNKV<Tkey, Tval> eTriple = read_data<TripleNKV<Tkey, Tval>>(socket);
       if (eTriple.nature == 'e') {
         std::cerr << "Terminating the Databank process\n";
         break;
@@ -266,50 +240,52 @@ DataBankServer(const bool& _Saving, const std::string& _SavingPrefix, const shor
         } else {
           if (Saving) {
             size_t n_orbit = ListEnt.size();
-            std::string Prefix = SavingPrefix + "DualDesc" + std::to_string(n_orbit);
+            std::string Prefix =
+                SavingPrefix + "DualDesc" + std::to_string(n_orbit);
             std::cerr << "Insert entry to file Prefix=" << Prefix << "\n";
             Write_BankEntry(Prefix, eTriple.eKey, eTriple.eVal);
           }
-          ListEnt.emplace(std::make_pair<Tkey, Tval>(std::move(eTriple.eKey), std::move(eTriple.eVal)));
+          ListEnt.emplace(std::make_pair<Tkey, Tval>(std::move(eTriple.eKey),
+                                                     std::move(eTriple.eVal)));
         }
       }
       if (eTriple.nature == 'g') {
-        std::cerr << "Passing by GetDualDesc |ListEnt|=" << ListEnt.size() << "\n";
-        typename std::unordered_map<Tkey, Tval>::const_iterator iter = ListEnt.find(eTriple.eKey);
+        std::cerr << "Passing by GetDualDesc |ListEnt|=" << ListEnt.size()
+                  << "\n";
+        typename std::unordered_map<Tkey, Tval>::const_iterator iter =
+            ListEnt.find(eTriple.eKey);
         if (iter == ListEnt.end())
-          send_data<Tval>(socket, Tval()); // If returning empty then it means nothing has been found.
+          send_data<Tval>(socket, Tval()); // If returning empty then it means
+                                           // nothing has been found.
         send_data<Tval>(socket, iter->second);
       }
-      std::cerr << "------------------------------ " << n_iter << " ------------------------------\n";
+      std::cerr << "------------------------------ " << n_iter
+                << " ------------------------------\n";
       n_iter++;
     }
   }
 };
 
-
-
-template<typename Tkey, typename Tval>
-struct DataBankClient {
+template <typename Tkey, typename Tval> struct DataBankClient {
 private:
   std::unordered_map<Tkey, Tval> ListEnt;
   short unsigned int port;
   boost::asio::ip::tcp::endpoint endpoint;
+
 public:
-  DataBankClient(const short unsigned int& _port) : port(_port), endpoint(boost::asio::ip::tcp::v4(), port)
-  {
+  DataBankClient(const short unsigned int &_port)
+      : port(_port), endpoint(boost::asio::ip::tcp::v4(), port) {}
+  void InsertEntry(Tkey &&eKey, Tval &&eVal) {
+    send_data_atomic<TripleNKV<Tkey, Tval>>(
+        endpoint, {'i', std::move(eKey), std::move(eVal)});
   }
-  void InsertEntry(Tkey && eKey, Tval && eVal)
-  {
-    send_data_atomic<TripleNKV<Tkey,Tval>>(endpoint, {'i', std::move(eKey), std::move(eVal)});
-  }
-  Tval GetDualDesc(const Tkey& eKey) const
-  {
+  Tval GetDualDesc(const Tkey &eKey) const {
     boost::asio::io_service io_service;
     boost::asio::ip::tcp::socket socket(io_service);
     socket.connect(endpoint);
-    send_data<TripleNKV<Tkey,Tval>>(socket, {'g', std::move(eKey), Tval()});
+    send_data<TripleNKV<Tkey, Tval>>(socket, {'g', std::move(eKey), Tval()});
     return read_data<Tval>(socket);
   }
 };
 
-#endif
+#endif // SRC_DUALDESC_DATABANK_H_
