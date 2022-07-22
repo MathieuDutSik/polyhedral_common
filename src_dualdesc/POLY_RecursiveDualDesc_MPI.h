f115d5fc8f3cf4aec0c3987d33160a6d283c909e
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-07-22 12:24:08 +0200

    More code insertions.

--- src_dualdesc/POLY_RecursiveDualDesc_MPI.h
+++ src_dualdesc/POLY_RecursiveDualDesc_MPI.h
@@ -288,30 +288,33 @@ vectface MPI_Kernel_DUALDESC_AdjacencyDecomposition(
     std::string const &ePrefix,
     std::map<std::string, typename Tgroup::Tint> const &TheMap) {
   using DataFacet = typename TbasicBank::DataFacet;
+  //  using TbasicBank = DatabaseCanonic<T, Tint, Tgroup>;
+  SingletonTime start;
+  int irank = comm.rank();
+  int n_proc = comm.size();
   std::ostream& os = get_standard_outstream(comm);
   DatabaseOrbits<TbasicBank> RPL(bb, ePrefix, AllArr.Saving, os);
-
-
+  //
+  // The types of exchanges
+  //
+  const int tag_new_facets =
+      36; // New facets to be added, the most common request
+  // tag_balinski
+  const int tag_balinski_info = 38;
+  // undone information for Balinski termination
+  const int tag_termination = 38;
+  const int expected_recv_message = 2047;
+  // undone information for Balinski termination
+  const int tag_setup_databank = 39;
+  //
+  // Reading the input
+  //
   int MaxNumberFlyingMessage =
       BlDATA.ListIntValues.at("MaxNumberFlyingMessage");
   int MaxIncidenceTreating = BlDATA.ListIntValues.at("MaxIncidenceTreating");
   int MaxStoredUnsentMatrices =
       BlDATA.ListIntValues.at("MaxStoredUnsentMatrices");
-  int MinIncidenceRealized = BlDATA.ListIntValues.at("MinIncidenceRealized");
-  int MaxIncidenceRealized = BlDATA.ListIntValues.at("MaxIncidenceRealized");
   int MaxRunTimeSecond = BlDATA.ListIntValues.at("MaxRunTimeSecond");
-
-
-  SingletonTime start;
-  int irank = comm.rank();
-
-  size_t n_orb_tot = 0, n_orb_loc = RPL.FuncNumberOrbit();
-  all_reduce(world, n_orb_loc, n_orb_tot, mpi::minimum<size_t>());
-  if (n_orb_tot > 0) {
-    std::string ansSamp = HeuristicEvaluation(TheMap, AllArr.InitialFacetSet);
-    for (auto &face : RPL.ComputeInitialSet(ansSamp))
-      RPL.FuncInsert(face);
-  }
 
 
 
@@ -334,50 +337,16 @@ vectface MPI_Kernel_DUALDESC_AdjacencyDecomposition(
     return -1;
   };
 
-    int nbCaseIncidence = MaxIncidenceRealized + 1 - MinIncidenceRealized;
-  std::vector<std::unordered_map<TypePerfectExch<Tint>, KeyData>>
-      ListCasesNotDone(nbCaseIncidence);
-  std::unordered_map<TypePerfectExch<Tint>, KeyData> ListCasesDone;
-  int idxMatrixCurrent = 0;
-  auto fInsert = [&](PairExch<Tint> const &ePair) -> void {
-    TypePerfectExch<Tint> ePerfect = ePair.ePerfect;
-    auto it1 = ListCasesDone.find(ePerfect);
-    if (it1 != ListCasesDone.end()) {
-      return;
-    }
-    int pos = ePerfect.incd - MinIncidenceRealized;
-    auto it2 = ListCasesNotDone[pos].find(ePerfect);
-    if (it2 != ListCasesNotDone[pos].end()) {
-      return;
-    }
-    ListCasesNotDone[pos][ePerfect] = {idxMatrixCurrent};
-    idxMatrixCurrent++;
-  };
-  
 
-  auto GetLowestIncidenceUndone =
-      [&]() -> boost::optional<std::pair<TypePerfectExch<Tint>, int>> {
-    for (int iCaseIncidence = 0; iCaseIncidence < nbCaseIncidence;
-         iCaseIncidence++) {
-      int incd = iCaseIncidence + MinIncidenceRealized;
-      if (incd <= MaxIncidenceTreating) {
-        auto it1 = ListCasesNotDone[iCaseIncidence].begin();
-        if (it1 != ListCasesNotDone[iCaseIncidence].end()) {
-          std::pair<TypePerfectExch<Tint>, int> ePair = {it1->first,
-                                                         it1->second.idxMatrix};
-          return boost::optional<std::pair<TypePerfectExch<Tint>, int>>(ePair);
-        }
-      }
-    }
-    return {};
-  };
+  std::vector<vectface> ListFaceUnsent(n_proc, vectface(40));
+
+
   auto SetMatrixAsDone = [&](TypePerfectExch<Tint> const &TheMat) -> void {
     int pos = TheMat.incd - MinIncidenceRealized;
     KeyData eKey = ListCasesNotDone[pos].at(TheMat);
     ListCasesNotDone[pos].erase(TheMat);
     ListCasesDone[TheMat] = eKey;
   };
-  
   auto fSendMatrix = [&](PairExch<Tint> const &ePair, int const &u) -> void {
     int res = IntegerDiscriminantInvariant(ePair.ePerfect.eMat, size);
     ListRequest[u] = world.isend(res, tag_new_form, ePair);
@@ -397,215 +366,69 @@ vectface MPI_Kernel_DUALDESC_AdjacencyDecomposition(
       pos--;
     }
   };
-  auto fInsertUnsent = [&](PairExch<Tint> const &ePair) -> void {
-    int res = IntegerDiscriminantInvariant(ePair.ePerfect.eMat, size);
+  auto fInsertUnsent = [&](Face const &face) -> void {
+    int res = IntegerDiscriminantInvariant(face) % n_proc;
     if (res == irank) {
-      fInsert(ePair);
+      RPL.FuncInsert(face);
     } else {
-      ListMatrixUnsent.push_back(ePair);
+      ListFaceUnsent[res].push_back(face);
       ClearUnsentAsPossible();
     }
   };
-  
-  
-  
-  int nbCaseNotDone = 0;
-  for (int iMatStart = 0; iMatStart < nbMatrixStart; iMatStart++) {
-    int eStatus;
-    is >> eStatus;
-    int incd;
-    is >> incd;
-    MyMatrix<Tint> TheMat = ReadMatrix<Tint>(is);
-    TypePerfectExch<Tint> eRecMat{incd, TheMat};
-    int res = IntegerDiscriminantInvariant(TheMat, size);
-    if (res == irank) {
-      KeyData eData{idxMatrixCurrent};
-      if (eStatus == 0) {
-        int pos = incd - MinIncidenceRealized;
-        ListCasesNotDone[pos][eRecMat] = eData;
-        nbCaseNotDone++;
-      } else {
-        ListCasesDone[eRecMat] = eData;
-      }
-      log << "Reading existing matrix=" << eRecMat
-          << " idxMatrixCurrent=" << idxMatrixCurrent << "END" << std::endl;
-      idxMatrixCurrent++;
-    }
+  //
+  // Initial invocation of the synchronization code
+  //
+  size_t n_orb_tot = 0, n_orb_loc = RPL.FuncNumberOrbit();
+  all_reduce(world, n_orb_loc, n_orb_tot, mpi::minimum<size_t>());
+  if (n_orb_tot > 0) {
+    std::string ansSamp = HeuristicEvaluation(TheMap, AllArr.InitialFacetSet);
+    for (auto &face : RPL.ComputeInitialSet(ansSamp))
+      fInsertUnsent(face);
   }
-  
+  //
+  // The infinite loop
+  //
   while (true) {
     boost::optional<boost::mpi::status> prob = world.iprobe();
     if (prob) {
-      std::cerr << "We are probing something\n";
       if (prob->tag() == tag_new_form) {
-        PairExch<Tint> ePair;
-        world.recv(prob->source(), prob->tag(), ePair);
-        fInsert(ePair);
+        vectface l_recv_face(40);
+        world.recv(prob->source(), prob->tag(), l_recv_face);
+        for (auto & face : l_recv_face)
+          RPL.FuncInsert(face);
+      }
+      if (prob->tag() == tag_termination) {
+        int recv_message;
+        comm.recv(prob->source(), prob->tag(), recv_message);
+        if (recv_message != expected_recv_message) {
+          std::cerr << "The recv_message is incorrect\n";
+          throw TerminalException{1};
+        }
       }
     } else {
-      std::cerr << "irank=" << irank
-                << " |ListMatrixUnsent|=" << ListMatrixUnsent.size()
-                << " MaxStoredUnsentMatrices=" << MaxStoredUnsentMatrices
-                << "\n";
-      if (int(ListMatrixUnsent.size()) < MaxStoredUnsentMatrices) {
-        boost::optional<std::pair<TypePerfectExch<Tint>, int>> eReq =
-            GetLowestIncidenceUndone();
-        if (eReq) {
-          std::cerr << "irank=" << irank << " eReq is non zero\n";
-          SetMatrixAsDone(eReq->first);
-          std::cerr << "irank=" << irank << " ePerfect=" << eReq->first << "\n";
-          MyMatrix<T> eMat_T =
-              UniversalMatrixConversion<T, Tint>(eReq->first.eMat);
-          int idxMatrixF = eReq->second;
-          std::cerr << "irank=" << irank << " Starting Adjacent Form Method\n";
-          std::vector<TypePerfectExch<Tint>> ListAdjacentObject =
-              GetAdjacentObjects<T, Tint>(eReq->first);
-          int nbAdjacent = ListAdjacentObject.size();
-          log << "Number of Adjacent for idxMatrixF=" << idxMatrixF
-              << " nbAdjacent=" << nbAdjacent << " END" << std::endl;
-          std::cerr << "irank=" << irank
-                    << " Number of Adjacent for idxMatrixF=" << idxMatrixF
-                    << " nbAdjacent=" << nbAdjacent << " END\n";
-          int iAdj = 0;
-          for (auto &eObj1 : ListAdjacentObject) {
-            TypeIndex eIndex{irank, idxMatrixF, iAdj};
-            PairExch<Tint> ePair{eObj1, eIndex};
-            fInsertUnsent(ePair);
-            iAdj++;
-          }
-        }
+      DataFacet df = RPL.FuncGetMinimalUndoneOrbit();
+      size_t SelectedOrbit = df.SelectedOrbit;
+      std::string NewPrefix =
+        ePrefix + "ADM" + std::to_string(SelectedOrbit) + "_";
+      vectface TheOutput =
+        DUALDESC_AdjacencyDecomposition<Tbank, T, Tgroup, Tidx_value>(TheBank, df.FF.EXT_face, df.Stab, AllArr, NewPrefix);
+      for (auto &eOrbB : TheOutput) {
+        Face eFlip = df.flip(eOrbB);
+        fInsertUnsent(eFlip);
       }
     }
     os << "Before ClearUnsentAsPossible\n";
     ClearUnsentAsPossible();
     os << "After ClearUnsentAsPossible\n";
     int elapsed_seconds = si(start);
-    if (MaxRunTimeSecond > 0) {
-      if (elapsed_seconds > MaxRunTimeSecond) {
-        os << "Exiting because the runtime is higher than the one expected\n";
-        break;
-      }
+    if (MaxRunTimeSecond > 0 && elapsed_seconds > MaxRunTimeSecond) {
+      os << "Exiting because the max_runtime is reached\n";
+      break;
     }
   }
 
-
-
-  
-  using Tint = typename Tgroup::Tint;
-  int irank = comm.rank();
-  int size = comm.size();
-  // New Facets (processed asynchronously)
-  const int tag_new_facets =
-      36; // New facets to be added, the most common request
-  // Message query being received
-  const int tag_message_query = 37;
-  // undone information for Balinski termination
-  const int tag_balinski_info = 38;
-  // undone information for Balinski termination
-  const int tag_terminate_send_vf = 38;
-  // undone information for Balinski termination
-  const int tag_setup_databank = 39;
-
-  using TbasicBank = DatabaseCanonic<T, Tint, Tgroup>;
   //  using DataFacet = typename TbasicBank::DataFacet;
-  struct BigRecordEntry {
-    std::vector<size_t> subset_index_proc;
-    DatabaseOrbits<TbasicBank> databank;
-    bool did_something;
-    std::vector<UndoneOrbitInfo<Tint>> list_undoneinfo;
-    int initiating_proc;
-  };
 
-  struct SetupDatabank {
-    MyMatrix<T> EXT;
-    Tgroup GRP;
-    size_t e_hash;
-  };
-
-
-
-
-
-  
-  // We can have DatabseBank created on disjoint processes.
-  // The order will not be the same between processors.
-  std::vector<BigRecordEntry> ListRPL;
-  TbasicBank bb(EXT, GRP);
-  ListRPL.emplace_back({
-      get_subset_index_rev(EXT.rows()),
-      DatabaseOrbits<TbasicBank>(bb, ePrefix, AllArr.Saving, std::cerr), false,
-      std::vector<UndoneOrbitInfo<Tint>>(size,
-                                         get_default_undoneinfo(EXT.rows())),
-      -1 // no initiating for the main one
-  });
-  std::unordered_map<size_t, uint8_t>
-      map_databank; // mapping from the hash of database orbit to the index in
-                    // ListRPL
-  std::vector<size_t> map_databank_rev; // mapping from index of ListRPL to hash
-  map_databank[0] = 0;
-  map_databank_rev.push_back(0);
-  uint8_t selected_pos = 0;
-  size_t selected_hash = 0;
-  auto get_pos = [&](auto &x) -> uint8_t { return map_databank[x.e_hash]; };
-  auto set_selected_pos = [&]() -> void {
-    int min_dim = std::numeric_limits<int>::max();
-    for (size_t i = 0; i < ListRPL.size(); i++) {
-      int dim = ListRPL[i].databank.EXT.cols();
-      if (dim < min_dim) {
-        selected_pos = i;
-        min_dim = dim;
-      }
-    }
-    selected_hash = map_databank_rev[selected_pos];
-  };
-  auto remove_databank = [&](const size_t &e_hash, const uint8_t &pos) -> void {
-    // Correct ListRPL
-    ListRPL.erase(pos);
-    // Correct map_databank
-    map_databank.erase(e_hash);
-    for (auto &kv : map_databank) {
-      if (kv.second > pos)
-        map_databank[kv.first] = kv.second - 1;
-    }
-    // Correct map_databank
-    for (size_t i = pos + 1; i < map_databank_rev.size(); i++)
-      map_databank_rev[i - 1] = map_databank_rev[i];
-    map_databank_rev.pop_back();
-    // Recomputed working_pos
-    set_selected_pos();
-  };
-  auto insert_databank = [&](int init_proc,
-                             const SetupDatabank &setup_db) -> void {
-    // Update ListRPL
-    TbasicBank bb(setup_db.EXT,
-                  setup_db.GRP); // Bugged as we are passing a reference to a
-                                 // temporary object.
-    ListRPL.emplace_back(
-        {get_subset_index_rev(setup_db.EXT.rows()),
-         DatabaseOrbits<TbasicBank>(bb, ePrefix, AllArr.Saving, std::cerr),
-         false,
-         std::vector<UndoneOrbitInfo<Tint>>(
-             size, get_default_undoneinfo(setup_db.EXT.rows())),
-         init_proc});
-    uint8_t pos = ListRPL.size();
-    // map_databank
-    map_databank[setup_db.e_hash] = pos;
-    // map_databank_rev
-    map_databank_rev.push_back(setup_db.e_hash);
-    // Recomputed working_pos
-    set_selected_pos();
-  };
-  // The Buffers in output and receive
-  std::vector<message_facet> ListEntries_OUT(size);
-  std::vector<message_facet> ListEntries_IN;
-  // The queries
-  std::vector<std::pair<int, message_query>> ListMesgQuery;
-  // The entry nbundone
-  std::vector<std::pair<int, HashUndoneOrbitInfo<Tint>>> ListMesgUndone;
-  // The partial dual desc (at the end of computation)
-  std::vector<std::pair<int, message_facet>> ListMesgPartDualDesc;
-  // The bank setup
-  std::vector<std::pair<int, SetupDatabank>> ListSetupDatabank;
   // The MPI related stuff
   std::vector<boost::mpi::request> ListRequest;
   std::vector<int> RequestStatus;
