5a85757d89a47ddd306db0e38f377525f5e19309
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-10-19 08:58:18 +0200

    Insert one more file for Vedran Krcadinac.

---
+++ src_latt/LATT_ZeroOneSolutions.cpp
@@ -0,0 +1,67 @@
+// Copyright (C) 2022 Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
+#include "NumberTheory.h"
+#include "SimulDiophantApprox.h"
+
+
+template<typename T, typename F>
+void DoProcessing(MyMatrix<T> const& M, MyVector<T> const& B, F f)
+{
+
+
+
+  
+}
+
+
+
+
+
+
+int main(int argc, char *argv[]) {
+  try {
+    if (argc != 3) {
+      std::cerr << "Number of argument is = " << argc << "\n";
+      std::cerr << "This program is used as\n";
+      std::cerr << "LATT_ZeroOneSolutions [FileIn] [epsilon]\n";
+      std::cerr << "\n";
+      std::cerr << "FileIn  : The vector file on on input\n";
+      std::cerr << "epsilon : The epsilon value on input\n";
+      return -1;
+    }
+    using T = mpq_class;
+    using Tint = mpz_class;
+    //
+    std::string FileI = argv[1];
+    std::string FileO = argv[1];
+    std::ifstream is(FileI);
+    std::ofstream os(FileO);
+
+    int nbRow, nbCol, contVal;
+    is >> nbRow;
+    is >> nbCol;
+    is >> contVal;
+    MyMatrix<T> M(nbRow, nbCol);
+    MyVector<T> B(nbRow);
+    for (int iRow=0; iRow<nbRow; iRow++) {
+      T val;
+      for (int iCol=0; iCol<nbCol; iCol++) {
+        is >> val;
+        M(iRow,iCol) = val;
+      }
+      is >> val;
+      B(iRow) = val;
+    }
+
+
+    auto f=[&](Face f) -> void {
+      for (int iCol=0; iCol<nbCol; iCol++) {
+        int val = f[iCol];
+        os << val;
+      }
+      os << "\n";
+    };
+    DoProcessing(M, B, f);
+  } catch (TerminalException const &e) {
+    exit(e.eVal);
+  }
+}
