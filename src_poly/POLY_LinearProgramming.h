8fc130e1a6a35e7850e98a3ced86ca77740e28ad
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-03-13 21:40:36 +0100

    Insert more debug stuff.

--- src_poly/POLY_LinearProgramming.h
+++ src_poly/POLY_LinearProgramming.h
@@ -914,31 +914,38 @@ std::optional<MyVector<T>> SolutionMatNonnegative_Check(MyMatrix<T> const &ListV
 template <typename T>
 std::optional<MyVector<T>> SolutionMatNonnegative_Safe(MyMatrix<T> const &ListVect,
                                                        MyVector<T> const &eVect) {
+  auto local_terminate=[&]() -> void {
+    std::string FILE_FAC = "DEBUG_Nonnegative_FAC";
+    WriteMatrixFile(FILE_FAC, ListVect);
+    std::string FILE_INEQ = "DEBUG_Nonnegative_INEQ";
+    WriteVectorFile(FILE_INEQ, eVect);
+    throw TerminalException{1};
+  };
   std::optional<MyVector<T>> opt_LP = SolutionMatNonnegative_LP(ListVect, eVect);
   if (opt_LP) {
     MyVector<T> const& V = *opt_LP;
     int nbRow = V.size();
     if (nbRow != ListVect.rows()) {
       std::cerr << "Dimension incoherency\n";
-      throw TerminalException{1};
+      local_terminate();
     }
     for (int iRow=0; iRow<nbRow; iRow++) {
       if (V(iRow) < 0) {
         std::cerr << "V should be non-negative\n";
-        throw TerminalException{1};
+        local_terminate();
       }
     }
     MyVector<T> prod = ListVect.transpose() * V;
     if (prod != eVect) {
       std::cerr << "prod does not matcheVect\n";
-      throw TerminalException{1};
+      local_terminate();
     }
     return V;
   }
   std::optional<MyVector<T>> opt_V1 = SolutionMatNonnegative_Version1(ListVect, eVect);
   if (opt_V1) {
     std::cerr << "opt_V1 is defined but not opt_LP, incoherent\n";
-    throw TerminalException{1};
+    local_terminate();
   }
   return {};
 }
