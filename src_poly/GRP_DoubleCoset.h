ccd019771bd394a149e7948cfa75c6d78d5b1c9c
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-03-22 09:44:31 +0100

    Some improvement to the double cosets tracking.

--- src_poly/GRP_DoubleCoset.h
+++ src_poly/GRP_DoubleCoset.h
@@ -264,7 +264,9 @@ void PrintDoubleCosetCasesTestProblem(Tgroup const &BigGRP, Tgroup const &SmaGRP
   std::string strSizeSma = std::to_string(SmaGRP.size());
   std::string strSizeBig = std::to_string(BigGRP.size());
   std::string strLen = std::to_string(eListBig.size());
-  std::string FileOut = "DoubleCoset_" + strSizeBig + "_" + strSizeSma + "_" + strLen;
+  std::string strN = std::to_string(int(BigGRP.n_act()));
+  std::string Prefix = "DoubleCoset_n" + strN + "_big" + strSizeBig + "_sma" + strSizeSma + "_vf" + strLen + "_idx";
+  std::string FileOut = FindAvailableFileFromPrefix(Prefix);
   std::ofstream os(FileOut);
   WriteGroup(os, BigGRP);
   WriteGroup(os, SmaGRP);
