b99ab1c831929ea4ecfb5f2498bcee48ca280877
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-04-12 14:31:24 +0200

    Update to the script.

--- Exmpl_Bench/Perfect_Group_65_1/run_benchmark.sh
+++ Exmpl_Bench/Perfect_Group_65_1/run_benchmark.sh
@@ -3,4 +3,4 @@
 rm -rf Saving_Bank && mkdir Saving_Bank
 rm -rf Saving_Polyhedral && mkdir Saving_Polyhedral
 
-time (./POLY_SerialDualDesc Perfect.nml 2>&1 | tee err2_benchmark)
+time (../../src_dualdesc/POLY_SerialDualDesc Perfect.nml 2>&1 | tee err2_benchmark)
