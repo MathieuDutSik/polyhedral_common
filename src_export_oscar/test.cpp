c8efceb778f54c3082a07d2e89eb60d0ae7990ce
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-04-20 10:59:58 +0200

    Insert new testing file.

---
+++ src_export_oscar/test.cpp
@@ -0,0 +1,17 @@
+#include "NumberTheory.h"
+#include "MAT_Matrix.h"
+int main()
+{
+  using T = mpq_class;
+
+  int n = 10;
+  int m = 10;
+  MyMatrix<T> M(n,m);
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<m; j++) {
+      M(i,j) = rand() % 2;
+    }
+  }
+  int rnk = RankMat(M);
+  std::cerr << "rnk=" << rnk << "\n";
+}
