1e79e707dcbc64a06594eb953c0484b347fe3612
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-04-28 17:03:24 +0200

    Sampling faet code update

--- src_poly/POLY_sampling_facets.cpp
+++ src_poly/POLY_sampling_facets.cpp
@@ -11,29 +11,47 @@
 // clang-format on
 
 template <typename T>
-void process(std::string const &eFileI, std::string const& ansSamp, std::ostream &os) {
+void process(std::string const &eFileI, std::string const& ansSamp, std::string const& OutFormat, std::ostream &os) {
   MyMatrix<T> EXT = ReadMatrixFile<T>(eFileI);
   vectface vf = DirectComputationInitialFacetSet(EXT, ansSamp, std::cerr);
-  os << "return ";
-  VectVectInt_Gap_Print(os, vf);
-  os << ";\n";
+  if (OutFormat == "GAP") {
+    os << "return ";
+    VectVectInt_Gap_Print(os, vf);
+    os << ";\n";
+    return;
+  }
+  if (OutFormat == "Oscar") {
+    size_t n_ent = vf.size();
+    size_t n = vf.size();
+    MyMatrix<int> M(n_ent, n);
+    for (size_t i_ent=0; i_ent<n_ent; i_ent++) {
+      Face f = vf[i_ent];
+      for (size_t i=0; i<n; i++)
+        M(i_ent, i) = f[i];
+    }
+    WriteMatrix(os, M);
+    return;
+  }
+  std::cerr << "Failed to find a matching entry for OutFormat=" << OutFormat << "\n";
+  throw TerminalException{1};
 }
 
 int main(int argc, char *argv[]) {
   HumanTime time1;
   try {
-    if (argc != 4 && argc != 5) {
+    if (argc != 4 && argc != 6) {
       std::cerr << "Number of argument is = " << argc << "\n";
       std::cerr << "This program is used as\n";
-      std::cerr << "POLY_sampling_facets arith command [DATAIN] [DATAOUT]\n";
+      std::cerr << "POLY_sampling_facets arith command [FileI] [OutFormat] [FileO]\n";
       std::cerr << "or\n";
-      std::cerr << "POLY_sampling_facets arith command [DATAIN]\n";
+      std::cerr << "POLY_sampling_facets arith command [FileI]\n";
       std::cerr << "\n";
       std::cerr << "with:\n";
-      std::cerr << "arith   : the chosen arithmetic\n";
-      std::cerr << "command : the program used for computing the dual description\n";
-      std::cerr << "DATAIN  : The polyhedral cone inequalities\n";
-      std::cerr << "DATAOUT : The file of output (if present, otherwise std::cerr)\n";
+      std::cerr << "arith     : the chosen arithmetic\n";
+      std::cerr << "command   : the program used for computing the dual description\n";
+      std::cerr << "FileI     : The polyhedral cone inequalities\n";
+      std::cerr << "OutFormat : The format of output, GAP or Oscar\n";
+      std::cerr << "FileO     : The file of output (if present, otherwise std::cerr)\n";
       std::cerr << "\n";
       std::cerr << "        --- arith ---\n";
       std::cerr << "\n";
@@ -63,20 +81,23 @@ int main(int argc, char *argv[]) {
     std::string arith = argv[1];
     std::string command = argv[2];
     std::string eFileI = argv[3];
+    std::string OutFormat = "GAP";
     std::string eFileO = "stderr";
-    if (argc == 5)
-      eFileO = argv[4];
+    if (argc == 6) {
+      OutFormat = argv[4];
+      eFileO = argv[5];
+    }
     auto call_lrs = [&](std::ostream &os) -> void {
       if (arith == "rational") {
-        return process<Trat>(eFileI, command, os);
+        return process<Trat>(eFileI, command, OutFormat, os);
       }
       if (arith == "Qsqrt5") {
         using T = QuadField<Trat, 5>;
-        return process<T>(eFileI, command, os);
+        return process<T>(eFileI, command, OutFormat, os);
       }
       if (arith == "Qsqrt2") {
         using T = QuadField<Trat, 2>;
-        return process<T>(eFileI, command, os);
+        return process<T>(eFileI, command, OutFormat, os);
       }
       std::optional<std::string> opt_realalgebraic =
           get_postfix(arith, "RealAlgebraic=");
@@ -91,7 +112,7 @@ int main(int argc, char *argv[]) {
         int const idx_real_algebraic_field = 1;
         insert_helper_real_algebraic_field(idx_real_algebraic_field, hcrf);
         using T = RealField<idx_real_algebraic_field>;
-        return process<T>(eFileI, command, os);
+        return process<T>(eFileI, command, OutFormat, os);
       }
       std::cerr << "Failed to find a matching field for arith=" << arith
                 << "\n";
