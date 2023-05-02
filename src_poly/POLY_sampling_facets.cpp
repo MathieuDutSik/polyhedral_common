f9ce5e5fc918d5acb0168c02444e70b652e273de
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-05-02 09:56:20 +0200

    Address another bug.

--- src_poly/POLY_sampling_facets.cpp
+++ src_poly/POLY_sampling_facets.cpp
@@ -22,7 +22,7 @@ void process(std::string const &eFileI, std::string const& ansSamp, std::string 
   }
   if (OutFormat == "Oscar") {
     size_t n_ent = vf.size();
-    size_t n = vf.size();
+    size_t n = vf.get_n();
     MyMatrix<int> M(n_ent, n);
     for (size_t i_ent=0; i_ent<n_ent; i_ent++) {
       Face f = vf[i_ent];
