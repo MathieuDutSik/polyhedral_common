3fb1218c09416e4aecf467dd731628b5a6daec19
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-06-05 11:46:37 +0200

    Upgrade to the code.

--- src_short/SHORT_ShortestConfig.h
+++ src_short/SHORT_ShortestConfig.h
@@ -8,6 +8,7 @@
 #include "MAT_MatrixInt.h"
 #include "MAT_functions.h"
 #include "MatrixGroup.h"
+#include "Parallel_Classes_Types.h"
 #include "POLY_LinearProgramming.h"
 #include "POLY_LinearProgramming_GLPK.h"
 #include "POLY_PolytopeInt.h"
