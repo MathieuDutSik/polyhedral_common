7d8348d597fd3c74ded49b05fdef7fb2abe4caad
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-06-06 20:04:22 +0200

    Upgrade.

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-#(cd src_poly && make clean && make)
-#(cd src_poly && make -f Makefile_double_cddlib clean && make -f Makefile_double_cddlib)
-#(cd src_polydecomp && make clean && make)
+(cd src_poly && make clean && make)
+(cd src_poly && make -f Makefile_double_cddlib clean && make -f Makefile_double_cddlib)
+(cd src_polydecomp && make clean && make)
 (cd src_latt && make clean && make)
 (cd src_short && make clean && make)
 (cd src_dualdesc && make clean && make)
