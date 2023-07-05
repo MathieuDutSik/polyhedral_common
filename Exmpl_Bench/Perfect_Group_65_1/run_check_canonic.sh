6c8f6fb7455f7040225d2327784425fb191f4f4b
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-07-05 13:04:48 +0200

    More correction.

--- Exmpl_Bench/Perfect_Group_65_1/run_check_canonic.sh
+++ Exmpl_Bench/Perfect_Group_65_1/run_check_canonic.sh
@@ -3,4 +3,4 @@
 rm -rf Saving_Bank && mkdir Saving_Bank
 rm -rf Saving_Polyhedral && mkdir Saving_Polyhedral
 
-time (../../src_dualdesc/POLY_SerialDualDesc Perfect_canonic.nml 2>&1 | tee err2_benchmark_canonic)
+time (../../src_dualdesc/POLY_SerialDualDesc Perfect_canonic.nml 2>&1 | tee err2_check_canonic)
--- Exmpl_Bench/Perfect_Group_65_1/run_check_schema1.sh
+++ Exmpl_Bench/Perfect_Group_65_1/run_check_schema1.sh
@@ -3,7 +3,8 @@
 rm -rf Saving_Bank && mkdir Saving_Bank
 rm -rf Saving_Polyhedral && mkdir Saving_Polyhedral
 
-time (../../src_dualdesc/POLY_SerialDualDesc Perfect_schema1_stepA.nml 2>&1 | tee err2_benchmark_schema1_stepA)
+time (../../src_dualdesc/POLY_SerialDualDesc Perfect_schema1_stepA.nml 2>&1 | tee err2_check_schema1_stepA)
 
 
-time (../../src_dualdesc/POLY_SerialDualDesc Perfect_canonic_initial_triv.nml 2>&1 | tee err2_benchmark_schema1_stepB)
+time (../../src_dualdesc/POLY_SerialDualDesc Perfect_canonic_initial_triv.nml 2>&1 | tee err2_check_schema1_stepB)
+
