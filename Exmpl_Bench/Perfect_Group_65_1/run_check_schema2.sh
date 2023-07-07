a76e62ffe2ed08d44818d0eccaf2bcce3a6998cd
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-07-07 12:19:13 +0200

    Renaming for clarity.

--- Exmpl_Bench/Perfect_Group_65_1/run_check_schema2.sh
+++ Exmpl_Bench/Perfect_Group_65_1/run_check_schema2.sh
@@ -3,7 +3,7 @@
 rm -rf Saving_Bank && mkdir Saving_Bank
 rm -rf Saving_Polyhedral && mkdir Saving_Polyhedral
 
-time (../../src_dualdesc/POLY_SerialDualDesc Perfect_schema1_stepA.nml 2>&1 | tee err2_check_schema2_stepA)
-time (../../src_dualdesc/POLY_SerialDualDesc Perfect_schema1_stepA.nml 2>&1 | tee err2_check_schema2_stepB)
-time (../../src_dualdesc/POLY_SerialDualDesc Perfect_schema1_stepA.nml 2>&1 | tee err2_check_schema2_stepC)
-time (../../src_dualdesc/POLY_SerialDualDesc Perfect_schema1_stepA.nml 2>&1 | tee err2_check_schema2_stepD)
+time (../../src_dualdesc/POLY_SerialDualDesc Perfect_canonic_one_hour.nml 2>&1 | tee err2_check_schema2_stepA)
+time (../../src_dualdesc/POLY_SerialDualDesc Perfect_canonic_one_hour.nml 2>&1 | tee err2_check_schema2_stepA)
+time (../../src_dualdesc/POLY_SerialDualDesc Perfect_canonic_one_hour.nml 2>&1 | tee err2_check_schema2_stepA)
+time (../../src_dualdesc/POLY_SerialDualDesc Perfect_canonic_one_hour.nml 2>&1 | tee err2_check_schema2_stepA)
