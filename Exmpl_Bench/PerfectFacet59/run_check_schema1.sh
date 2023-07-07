386eb69d326d3b8c6e46de7f7ded46794975c2ad
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-07-07 13:56:24 +0200

    Renaming of input file.

--- Exmpl_Bench/PerfectFacet59/run_check_schema1.sh
+++ Exmpl_Bench/PerfectFacet59/run_check_schema1.sh
@@ -3,8 +3,6 @@
 rm -rf Saving_Bank && mkdir Saving_Bank
 rm -rf Saving_Polyhedral && mkdir Saving_Polyhedral
 
-time (../../src_dualdesc/POLY_SerialDualDesc Perfect_canonic_one_hour.nml 2>&1 | tee err2_check_schema1_stepA)
-
-
+time (../../src_dualdesc/POLY_SerialDualDesc Perfect_canonic_15min.nml 2>&1 | tee err2_check_schema1_stepA)
 time (../../src_dualdesc/POLY_SerialDualDesc Perfect_canonic_initial_triv.nml 2>&1 | tee err2_check_schema1_stepB)
 
