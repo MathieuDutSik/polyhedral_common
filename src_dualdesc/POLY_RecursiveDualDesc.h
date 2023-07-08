9bab76605ad63e4840280eed262655e4e62de6b7
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-07-08 09:07:45 +0200

    Likely resolve the bug.

--- src_dualdesc/POLY_RecursiveDualDesc.h
+++ src_dualdesc/POLY_RecursiveDualDesc.h
@@ -2063,7 +2063,7 @@ void vectface_update_method(vectface & vfo, TbasicBank & bb) {
     if (fo1 != fo2) {
       n_diff += 1;
     }
-    vfo[i_orbit] = fo;
+    vfo.SetFace(fo, i_orbit);
   }
   std::cerr << "vectface_update_method, n_diff=" << n_diff << "\n";
 }
