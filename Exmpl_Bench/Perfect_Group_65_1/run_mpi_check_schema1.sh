56cb13b8c873ec7257a545bf7ba5d5add593e161
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-07-08 09:39:56 +0200

    Correct script.

--- Exmpl_Bench/Perfect_Group_65_1/run_mpi_check_schema1.sh
+++ Exmpl_Bench/Perfect_Group_65_1/run_mpi_check_schema1.sh
@@ -2,8 +2,8 @@
 
 rm -f Result
 rm -f log_*
-rm -rf Saving_Bank*
-rm -rf Saving_Polyhedral*
+rm -rf Saving_Bank_nproc*_rank*
+rm -rf Saving_Polyhedral_nproc*_rank*
 
 mpirun -np 2 ../../src_dualdesc/POLY_MPI_DualDesc Perfect_mpi_canonic.nml
 mpirun -np 2 ../../src_dualdesc/POLY_MPI_DualDesc Perfect_mpi_canonic_initial_triv.nml
