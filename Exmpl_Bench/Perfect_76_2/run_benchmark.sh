ab60aa313e74d3d2e8251ed81362c726f639b19c
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-08-19 09:29:16 +0200

    Renaming.

--- Exmpl_Bench/Perfect_76_2/run_benchmark.sh
+++ Exmpl_Bench/Perfect_76_2/run_benchmark.sh
@@ -3,4 +3,4 @@
 rm -rf Saving_Bank && mkdir Saving_Bank
 rm -rf Saving_Polyhedral && mkdir Saving_Polyhedral
 
-../../src_dualdesc/POLY_SerialDualDesc Perfect.nml
+../../src_dualdesc/POLY_SerialDualDesc Perfect_time_limit.nml
