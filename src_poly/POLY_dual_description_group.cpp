493eb4ff79f23d21b17880a578ec2061b5abbb99
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-08-23 10:09:27 +0200

    Small change in documentation + Makefile.

--- src_poly/POLY_dual_description_group.cpp
+++ src_poly/POLY_dual_description_group.cpp
@@ -73,6 +73,7 @@ int main(int argc, char *argv[]) {
       std::cerr << "\n";
       std::cerr << "cdd      : the cdd program\n";
       std::cerr << "lrs      : the lrs program\n";
+      std::cerr << "pd_lrs   : the primal dual technique with lrs used for test\n";
       std::cerr << "lrs_ring : the lrs program but reduced to ring computation (remove denominators)\n";
       std::cerr << "glrs     : the external program glrs\n";
       std::cerr << "ppl_ext  : the external program ppl_ext\n";
