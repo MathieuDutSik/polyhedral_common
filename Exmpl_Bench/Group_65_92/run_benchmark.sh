813e1060f2a7bfb6d4af331d55e8b9b204c635e4
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-08-28 09:10:03 +0200

    Address some update to the code to accelerate it.

--- Exmpl_Bench/Group_65_92/run_benchmark.sh
+++ Exmpl_Bench/Group_65_92/run_benchmark.sh
@@ -3,5 +3,5 @@
 rm -rf Saving_Bank && mkdir Saving_Bank
 rm -rf Saving_Polyhedral && mkdir Saving_Polyhedral
 
-../../src_dualdesc/POLY_SerialDualDesc serial.nml
-../../src_dualdesc/POLY_SerialDualDesc serial.nml
+../../src_dualdesc/POLY_SerialDualDesc serial.nml 2> RUN1
+../../src_dualdesc/POLY_SerialDualDesc serial.nml 2> RUN2
