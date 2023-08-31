fd4a4003c207bc858042122bd876afa2ec1086c6
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-08-31 16:35:55 +0200

    Correct for unused variable.

--- src_poly/POLY_SamplingFacet.h
+++ src_poly/POLY_SamplingFacet.h
@@ -152,7 +152,7 @@ DUALDESC_SamplingFacetProcedure(MyMatrix<T> const &EXT,
 template <typename T>
 vectface Kernel_DirectComputationInitialFacetSet(MyMatrix<T> const &EXT,
                                                  std::string const &ansSamp,
-                                                 std::ostream &os) {
+                                                 [[maybe_unused]] std::ostream &os) {
 #ifdef TIMINGS_SAMPLING_FACET
   MicrosecondTime time;
 #endif
