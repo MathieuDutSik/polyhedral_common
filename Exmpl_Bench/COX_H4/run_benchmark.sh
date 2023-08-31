36bf9c4a54a7ec997f318816c94bdd8ba734b239
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-08-31 17:42:01 +0200

    Some changes.

--- Exmpl_Bench/COX_H4/run_benchmark.sh
+++ Exmpl_Bench/COX_H4/run_benchmark.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-rm -rf Saving_Bank && mkdir Saving_Bank
-rm -rf Saving_Polyhedral && mkdir Saving_Polyhedral
-
 time (../../src_dualdesc/POLY_SerialDualDesc FacetsConvH4.nml 2>&1 | tee err2_polydualdesc)
