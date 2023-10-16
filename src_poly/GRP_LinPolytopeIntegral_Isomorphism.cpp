821be1726893afbb370bdf65adf48b61c3ae992b
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-10-16 15:09:36 +0200

    Address the code presentation.

--- src_poly/GRP_LinPolytopeIntegral_Isomorphism.cpp
+++ src_poly/GRP_LinPolytopeIntegral_Isomorphism.cpp
@@ -7,58 +7,101 @@
 #include "Temp_PolytopeEquiStab.h"
 // clang-format on
 
+template<typename Tint>
+void process_A(std::string const& FileExt1, std::string const& FileExt2, std::string const& OutFormat, std::ostream & os) {
+  using Tidx = uint32_t;
+  using Telt = permutalib::SingleSidedPerm<Tidx>;
+  using Tgroup = permutalib::Group<Telt, Tint>;
+  using Tidx_value = uint32_t;
+  //    using Tgr = GraphBitset;
+  using Tgr = GraphListAdj;
+  MyMatrix<Tint> EXT1 = ReadMatrixFile<Tint>(FileExt1);
+  MyMatrix<Tint> EXT2 = ReadMatrixFile<Tint>(FileExt2);
+  size_t nbCol = EXT1.cols();
+  size_t nbRow = EXT1.rows();
+  std::cerr << "nbRow=" << nbRow << " nbCol=" << nbCol << "\n";
+  //
+  //    const bool use_scheme = true;
+  const bool use_scheme = false;
+  std::optional<MyMatrix<Tint>> equiv =
+    LinPolytopeIntegral_Isomorphism<Tint, Tidx, Tgroup, Tidx_value, Tgr,
+                                    use_scheme>(EXT1, EXT2);
+  if (OutFormat == "GAP") {
+    if (equiv) {
+      os << "return ";
+      WriteMatrixGAP(os, *equiv);
+      os << ";\n";
+    } else {
+      os << "return fail;\n";
+    }
+    return;
+  }
+  std::cerr << "Failed to find a matching OutFormat\n";
+  throw TerminalException{1};
+}
+
+
 int main(int argc, char *argv[]) {
   HumanTime time1;
   try {
-    if (argc != 4 && argc != 3) {
+    if (argc != 6 && argc != 4) {
       std::cerr << "Number of argument is = " << argc << "\n";
       std::cerr << "This program is used as\n";
-      std::cerr << "GRP_LinPolytope_Isomorphism [EXT1] [EXT2] [OutEquiv]\n";
+      std::cerr << "GRP_LinPolytopeIntegral_Isomorphism arith [EXT1] [EXT2] [OutFormat] [FileOut]\n";
       std::cerr << "or\n";
-      std::cerr << "GRP_LinPolytope_Isomorphism [EXT1] [EXT2]\n";
+      std::cerr << "GRP_LinPolytopeIntegral_Isomorphism arith [EXT1] [EXT2]\n";
       std::cerr << "\n";
-      std::cerr << "OutEquiv : The equivalence information file (otherwise "
-                   "printed to screen)\n";
+      std::cerr << "         ------ arith -------\n";
+      std::cerr << "\n";
+      std::cerr << "mpz_class   : The GMP class with the GMP binder\n";
+      std::cerr << "mpq_integer : The GMP class embedded into boost\n";
+      std::cerr << "\n";
+      std::cerr << "EXT1 and EXT2 are files of the input files\n";
+      std::cerr << "\n";
+      std::cerr << "         ----- OutFormat ------\n";
+      std::cerr << "\n";
+      std::cerr << "GAP: The output in the GAP readable file\n";
+      std::cerr << "\n";
+      std::cerr << "         ----- FileOut -----\n";
+      std::cerr << "\n";
+      std::cerr << "If stderr, or stdout, then output to standard error or standrd output\n";
+      std::cerr << "Other output to the designated file name\n";
       return -1;
     }
     //
     using Tint = mpz_class;
-    using Tidx = uint32_t;
-    using Telt = permutalib::SingleSidedPerm<Tidx>;
-    using Tint = mpz_class;
-    using Tgroup = permutalib::Group<Telt, Tint>;
-    using Tidx_value = uint32_t;
-    using Tgr = GraphBitset;
     //
-    std::string FileExt1 = argv[1];
-    std::string FileExt2 = argv[2];
-    MyMatrix<Tint> EXT1 = ReadMatrixFile<Tint>(FileExt1);
-    MyMatrix<Tint> EXT2 = ReadMatrixFile<Tint>(FileExt2);
-    size_t nbCol = EXT1.cols();
-    size_t nbRow = EXT1.rows();
-    std::cerr << "nbRow=" << nbRow << " nbCol=" << nbCol << "\n";
-    //
-    //    const bool use_scheme = true;
-    const bool use_scheme = false;
-    std::optional<MyMatrix<Tint>> equiv =
-        LinPolytopeIntegral_Isomorphism<Tint, Tidx, Tgroup, Tidx_value, Tgr,
-                                        use_scheme>(EXT1, EXT2);
-    //
-    auto print_info = [&](std::ostream &os) -> void {
-      if (equiv) {
-        os << "return ";
-        WriteMatrixGAP(os, *equiv);
-        os << ";\n";
-      } else {
-        os << "return fail;\n";
-      }
-    };
-    if (argc == 4) {
-      std::ofstream os(argv[3]);
-      print_info(os);
+    std::string arith = argv[1];
+    std::string FileExt1 = argv[2];
+    std::string FileExt2 = argv[3];
+    std::string OutFormat = "GAP";
+    std::string FileOut = "stderr";
+    if (argc == 6) {
+      OutFormat = argv[4];
+      FileOut = argv[5];
     }
-    if (argc == 3) {
-      print_info(std::cerr);
+    //
+    auto process_B=[&](std::ostream & os) -> void {
+      if (arith == "mpz_class") {
+        using Tint = mpz_class;
+        return process_A<Tint>(FileExt1, FileExt2, OutFormat, os);
+      }
+      if (arith == "mpq_integer") {
+        using Tint = mpz_class;
+        return process_A<Tint>(FileExt1, FileExt2, OutFormat, os);
+      }
+      std::cerr << "Failed to find a matching type for arith\n";
+      throw TerminalException{1};
+    };
+    if (FileOut == "stderr") {
+      process_B(std::cerr);
+    } else {
+      if (FileOut == "stdout") {
+        process_B(std::cout);
+      } else {
+        std::ofstream os(FileOut);
+        process_B(os);
+      }
     }
     std::cerr << "Normal termination of the program\n";
   } catch (TerminalException const &e) {
