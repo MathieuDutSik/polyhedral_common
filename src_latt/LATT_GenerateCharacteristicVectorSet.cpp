952eb70123e0a4a7e64daf820f73d13a4ae6237b
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-12-21 11:45:41 +0100

    Further corrections for compilation.

--- src_latt/LATT_GenerateCharacteristicVectorSet.cpp
+++ src_latt/LATT_GenerateCharacteristicVectorSet.cpp
@@ -57,7 +57,7 @@ void process_B(std::string const& arithmetic_vec, std::string choice, std::strin
 }
 
 void process_A(std::string const& arithmetic_mat, std::string const& arithmetic_vec, std::string choice, std::string MatFile, std::string OutFile) {
-  if (arithmetic_mat = "mpq_rational") {
+  if (arithmetic_mat == "mpq_rational") {
     using T = mpq_class;
     return process_B<T>(arithmetic_vec, choice, MatFile, OutFile);
   }
@@ -76,78 +76,12 @@ int main(int argc, char *argv[]) {
       std::cerr << "LATT_GenerateCharacteristicVectorSet arithmetic_mat arithmetic_vect choice [MaFilet] [OutFile]\n";
       return -1;
     }
-    using Tidx = uint32_t;
-    //
     std::string arithmetic_mat = argv[1];
     std::string arithmetic_vec = argv[2];
     std::string choice = argv[3];
     std::string MatFile = argv[4];
     std::string OutFile = argv[5];
-    std::vector<MyMatrix<T>> ListMat = ReadListMatrixFile<T>(FileListMat);
-
-    MyMatrix<Tint> SHV =
-        ExtractInvariantVectorFamilyZbasis<T, Tint>(ListMat[0]);
-
-    MyMatrix<T> SHV_T = UniversalMatrixConversion<T, Tint>(SHV);
-    int n_row = SHV_T.rows();
-    std::vector<T> Vdiag(n_row, 0);
-
-    const bool use_scheme = true;
-    std::vector<std::vector<Tidx>> ListGen =
-        GetListGenAutomorphism_ListMat_Vdiag<T, Tfield, Tidx, use_scheme>(
-            SHV_T, ListMat, Vdiag, std::cerr);
-
-    std::vector<MyMatrix<Tint>> ListGenEquiv;
-    for (auto &eList : ListGen) {
-      std::optional<MyMatrix<T>> opt =
-          FindMatrixTransformationTest(SHV_T, SHV_T, eList);
-      if (!opt) {
-        std::cerr << "Failed to find the matrix\n";
-        throw TerminalException{1};
-      }
-      MyMatrix<T> const &M_T = *opt;
-      if (!IsIntegralMatrix(M_T)) {
-        std::cerr << "Bug: The matrix should be integral\n";
-        throw TerminalException{1};
-      }
-      MyMatrix<Tint> M = UniversalMatrixConversion<Tint, T>(M_T);
-      ListGenEquiv.push_back(M);
-    }
-    //
-    auto prt = [&](std::ostream &os) -> void {
-      if (OutFormat == "GAP") {
-        os << "return [";
-        bool IsFirst = true;
-        for (auto &eMat : ListGenEquiv) {
-          if (!IsFirst)
-            os << ",\n";
-          IsFirst = false;
-          WriteMatrixGAP(os, eMat);
-        }
-        os << "];\n";
-        return;
-      }
-      if (OutFormat == "Oscar") {
-        os << ListGenEquiv.size() << "\n";
-        for (auto &eMat : ListGenEquiv) {
-          WriteMatrix(os, eMat);
-        }
-        return;
-      }
-      std::cerr << "Failed to find a matching type for OutFormat=" << OutFormat
-                << "\n";
-      throw TerminalException{1};
-    };
-    if (OutFile == "stderr") {
-      prt(std::cerr);
-    } else {
-      if (OutFile == "stdout") {
-        prt(std::cout);
-      } else {
-        std::ofstream os(OutFile);
-        prt(os);
-      }
-    }
+    process_A(arithmetic_mat, arithmetic_vec, choice, MatFile, OutFile);
     std::cerr << "Normal termination of LATT_Automorphism\n";
   } catch (TerminalException const &e) {
     std::cerr << "Raised exception led to premature end of LATT_Automorphism\n";
