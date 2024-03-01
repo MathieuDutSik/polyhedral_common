1a841fb846fef7327e1b15c6ad145b52e2b0785a
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-03-01 17:31:51 +0900

    Insert new code (need to be test for compilation and debugged).

---
+++ src_ctype_mpi/CTYP_MPI_AdjScheme.cpp
@@ -0,0 +1,46 @@
+// Copyright (C) 2022 Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
+// clang-format off
+#include "NumberTheory.h"
+#include "CtypeMPI_types.h"
+#include "CtypeMPI_enumeration.h"
+#include "Namelist.h"
+#include "hash_functions.h"
+#include "rational.h"
+#include "sparse-map/include/tsl/sparse_map.h"
+#include <unordered_map>
+// clang-format on
+namespace mpi = boost::mpi;
+
+int main(int argc, char *argv[]) {
+  boost::mpi::environment env(boost::mpi::threading::serialized);
+  if (env.thread_level() < boost::mpi::threading::serialized) {
+    env.abort(-1);
+  }
+  boost::mpi::communicator world;
+  HumanTime time;
+  using T = mpq_class;
+  using Tint = int;
+  using Tidx = uint16_t;
+  using Telt = permutalib::SingleSidedPerm<Tidx>;
+  using TintGroup = mpz_class;
+  using Tgroup = permutalib::Group<Telt, TintGroup>;
+  try {
+    Eigen::initParallel();
+    FullNamelist eFull = NAMELIST_GetStandard_COMPUTE_LATTICE_IsoEdgeDomains();
+    if (argc != 2) {
+      std::cerr << "Number of argument is = " << argc << "\n";
+      std::cerr << "This program is used as\n";
+      std::cerr << "CTYP_MPI_AdjScheme [file.nml]\n";
+      std::cerr << "With file.nml a namelist file\n";
+      NAMELIST_WriteNamelistFile(std::cerr, eFull, true);
+      return -1;
+    }
+    std::string eFileName = argv[1];
+    NAMELIST_ReadNamelistFile(eFileName, eFull);
+    ComputeLatticeIsoDelaunayDomains<T,Tint,Tgroup>(world, eFull);
+  } catch (TerminalException const &e) {
+    std::cerr << "Error in CTYP_MPI_AdjScheme\n";
+    exit(e.eVal);
+  }
+  runtime(time);
+}
