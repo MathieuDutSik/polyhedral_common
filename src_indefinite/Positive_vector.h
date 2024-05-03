b3fcefc7c12887a2a932f5f115dcc409d539acb1
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-05-03 10:49:11 +0200

    Some insert of attempt code.

---
+++ src_indefinite/Positive_vector.h
@@ -0,0 +1,31 @@
+// Copyright (C) 2024 Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
+#ifndef SRC_INDEFINITE_POSITIVE_VECTOR_H_
+#define SRC_INDEFINITE_POSITIVE_VECTOR_H_
+
+// clang-format off
+#include "GRP_DoubleCoset.h"
+#include <vector>
+// clang-format on
+
+
+
+// This code looks for a vector of positive norm for an indefinite form.
+// We are looking for vectors with small coefficients, though no guarantee
+// is provided on that point.
+
+template<typename T, typename Tint>
+MyVector<Tint> POS_VEC_DiagonalizeOperation(MyMatrix<T> const& M) {
+  
+}
+
+
+
+
+
+
+
+
+
+// clang-format off
+#endif  //  SRC_INDEFINITE_POSITIVE_VECTOR_H_
+// clang-format on
