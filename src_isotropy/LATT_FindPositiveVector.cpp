b39195707b7f683b4fe41b72273912b8ab821105
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-08-18 16:50:38 +0200

    Insert the test code for the positive vectors.

---
+++ src_isotropy/LATT_FindPositiveVector.cpp
@@ -0,0 +1,90 @@
+// Copyright (C) 202" Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
+// clang-format off
+#include "NumberTheoryCommon.h"
+#include "NumberTheoryGmp.h"
+#include "NumberTheorySafeInt.h"
+#include "Positivity.h"
+// clang-format on
+
+bool ParseBoolean(std::string const& strI) {
+  if (strI == "T") {
+    return true;
+  }
+  if (strI == "F") {
+    return false;
+  }
+  if (strI == "true") {
+    return true;
+  }
+  if (strI == "false") {
+    return false;
+  }
+  std::cerr << "ParseBoolean error: possible input, T, F, true, false\n";
+  throw TerminalException{1};
+}
+
+
+template <typename T, typename Tint>
+void process(std::string const &FileI, std::string const& strCritNorm, std::string const& strNeedNonZero,
+             std::string const &OutFormat, std::ostream &os_out) {
+  MyMatrix<T> M = ReadMatrixFile<T>(FileI);
+  T CritNorm = ParseScalar<T>(strCritNorm);
+  bool NeedNonZero = ParseBoolean(strNeedNonZero);
+  //
+  MyVector<Tint> V = GetIntegralVector_allmeth<T,Tint>(M, CritNorm, NeedNonZero, std::cerr);
+  if (OutFormat == "GAP") {
+    os_out << "return " << StringVectorGAP(V) << ";\n";
+    return;
+  }
+  std::cerr << "Failed to find a matching OutFormat\n";
+  throw TerminalException{1};
+}
+
+int main(int argc, char *argv[]) {
+  SingletonTime time1;
+  try {
+    if (argc != 5 && argc != 7) {
+      std::cerr << "LATT_FindPositiveVector arith [FileI] [CritNorm] [NeedNonZero] [OutFormat] [FileO]\n";
+      std::cerr << "or\n";
+      std::cerr << "LATT_FindPositiveVector arith [FileI] [CritNorm] [NeedNonZeero]\n";
+      std::cerr << "\n";
+      std::cerr << "Possibilities for arith: rational\n";
+      throw TerminalException{1};
+    }
+    std::string arith = argv[1];
+    std::string FileI = argv[2];
+    std::string strCritNorm = argv[3];
+    std::string strNeedNonZero = argv[4];
+    std::string OutFormat = "GAP";
+    std::string FileO = "stderr";
+    if (argc == 7) {
+      OutFormat = argv[5];
+      FileO = argv[6];
+    }
+    //
+    auto f = [&](std::ostream &os) -> void {
+      if (arith == "rational") {
+        using T = mpq_class;
+        using Tint = mpz_class;
+        return process<T,Tint>(FileI, strCritNorm, strNeedNonZero, OutFormat, os);
+      }
+      std::cerr << "Failed to find matching type for arith\n";
+      throw TerminalException{1};
+    };
+    if (FileO == "stderr") {
+      f(std::cerr);
+    } else {
+      if (FileO == "stdout") {
+        f(std::cout);
+      } else {
+        std::ofstream os(FileO);
+        f(os);
+      }
+    }
+    std::cerr << "Normal termination of the program\n";
+  } catch (TerminalException const &e) {
+    std::cerr << "Error in LATT_FindIsotropic\n";
+    exit(e.eVal);
+  }
+  runtime(time1);
+}
