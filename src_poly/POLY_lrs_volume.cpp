cd73f73190acb4844165b3f2cae24957154509fc
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-08-30 09:29:58 +0200

    Change the code for making it usable.

--- src_poly/POLY_lrs_volume.cpp
+++ src_poly/POLY_lrs_volume.cpp
@@ -8,18 +8,29 @@
 // clang-format on
 
 template <typename T>
-void process(std::string const &eFileI) {
+void process(std::string const &eFileI, std::string const& OutFormat, std::ostream& os) {
   MyMatrix<T> EXT = ReadMatrixFile<T>(eFileI);
   T volume = lrs::Kernel_VolumePolytope(EXT);
-  std::cerr << "volume=" << volume << "\n";
+  if (OutFormat == "Direct") {
+    os << "volume=" << volume << "\n";
+    return;
+  }
+  if (OutFormat == "GAP") {
+    os << "return " << volume << ";\n";
+    return;
+  }
+  std::cerr << "Failed to find a matching output\n";
+  throw TerminalException{1};
 }
 
 int main(int argc, char *argv[]) {
   HumanTime time1;
   try {
-    if (argc != 3) {
+    if (argc != 3 && argc != 5) {
       std::cerr << "Number of argument is = " << argc << "\n";
       std::cerr << "This program is used as\n";
+      std::cerr << "POLY_lrs_volume arith [DATAIN] [OutFormat] [FileOut]\n";
+      std::cerr << "\n";
       std::cerr << "POLY_lrs_volume arith [DATAIN]\n";
       std::cerr << "\n";
       std::cerr << "with:\n";
@@ -46,38 +57,44 @@ int main(int argc, char *argv[]) {
     //
     std::string arith = argv[1];
     std::string eFileI = argv[2];
-    auto call_lrs = [&]() -> void {
+    std::string OutFormat = "Direct";
+    std::string FileO = "stderr";
+    if (argc == 5) {
+      OutFormat = argv[3];
+      FileO = argv[4];
+    }
+    auto call_lrs = [&](std::ostream& os) -> void {
       if (arith == "safe_integer") {
         using T = SafeInt64;
-        return process<T>(eFileI);
+        return process<T>(eFileI, OutFormat, os);
       }
       /*
       if (arith == "safe_rational") {
         using T = Rational<SafeInt64>;
-        return process<T>(eFileI);
+        return process<T>(eFileI, OutFormat, os);
       }
       */
       if (arith == "integer") {
         using T = mpz_class;
-        return process<T>(eFileI);
+        return process<T>(eFileI, OutFormat, os);
       }
       if (arith == "rational") {
         using T = mpq_class;
-        return process<T>(eFileI);
+        return process<T>(eFileI, OutFormat, os);
       }
       if (arith == "double") {
         using T = double;
-        return process<T>(eFileI);
+        return process<T>(eFileI, OutFormat, os);
       }
       if (arith == "Qsqrt5") {
         using Trat = mpq_class;
         using T = QuadField<Trat, 5>;
-        return process<T>(eFileI);
+        return process<T>(eFileI, OutFormat, os);
       }
       if (arith == "Qsqrt2") {
         using Trat = mpq_class;
         using T = QuadField<Trat, 2>;
-        return process<T>(eFileI);
+        return process<T>(eFileI, OutFormat, os);
       }
       std::optional<std::string> opt_realalgebraic =
           get_postfix(arith, "RealAlgebraic=");
@@ -93,7 +110,7 @@ int main(int argc, char *argv[]) {
         int const idx_real_algebraic_field = 1;
         insert_helper_real_algebraic_field(idx_real_algebraic_field, hcrf);
         using T = RealField<idx_real_algebraic_field>;
-        return process<T>(eFileI);
+        return process<T>(eFileI, OutFormat, os);
       }
       std::cerr << "Failed to find a matching field for arith=" << arith
                 << "\n";
@@ -101,7 +118,16 @@ int main(int argc, char *argv[]) {
                    "RealAlgebraic\n";
       throw TerminalException{1};
     };
-    call_lrs();
+    if (FileO == "stderr") {
+      call_lrs(std::cerr);
+    } else {
+      if (FileO == "stdout") {
+        call_lrs(std::cout);
+      } else {
+        std::ofstream os(FileO);
+        call_lrs(os);
+      }
+    }
     std::cerr << "Normal termination of the program\n";
   } catch (TerminalException const &e) {
     std::cerr << "Error in POLY_lrs_volume\n";
