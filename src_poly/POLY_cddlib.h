e32649d22e18ce4ae4aba4c1e6711ed7b448d125
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-07-22 07:30:37 +0200

    Solve another memory leak.

--- src_poly/POLY_cddlib.h
+++ src_poly/POLY_cddlib.h
@@ -8749,6 +8749,7 @@ CDD_LinearProgramming_exact_V2(MyMatrix<T> const &EXT, MyVector<T> const &eVect,
     M->rowvec[j] = UniversalScalarConversion<Tfloat, T>(eVect(j));
   }
   lp = cdd::dd_Matrix2LP(M);
+  dd_FreeMatrix(M);
   lp->objective = cdd::dd_LPmin;
 #ifdef DEBUG_CDD
   os << "CDD: Before dd_LPSolve\n";
