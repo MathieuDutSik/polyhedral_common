87f407a989b599adeeba1067f1971a7fb972122d
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2026-04-05 12:00:33 +0200

    Some correction.

--- src_copos/CP_CopositiveListCone.cpp
+++ src_copos/CP_CopositiveListCone.cpp
@@ -7,10 +7,12 @@
 int main(int argc, char *argv[]) {
   HumanTime time1;
   try {
-    if (argc != 3) {
+    if (argc != 2 && argc != 4) {
       std::cerr << "Number of argument is = " << argc << "\n";
       std::cerr << "This program is used as\n";
-      std::cerr << "CP_CopositiveListCone [DATASYMM] [OUTFILE]\n";
+      std::cerr << "CP_CopositiveListCone [DATASYMM] [OutFormat] [OutFile]\n";
+      std::cerr << "or\n";
+      std::cerr << "CP_CopositiveListCone [DATASYMM]\n";
       std::cerr << "\n";
       std::cerr
           << "DATASYMM: The input data of the copositive symmetric matrix\n";
@@ -21,28 +23,45 @@ int main(int argc, char *argv[]) {
     using T = mpq_class;
     using Tint = mpz_class;
     //
-    std::cerr << "Reading input\n";
-    //
-    MyMatrix<T> eSymmMat = ReadMatrixFile<T>(argv[1]);
+    std::string FileI = argv[1];
+    std::string OutFormat = "GAP";
+    std::string FileO = "stderr";
+    if (argc == 4) {
+      OutFormat = argv[2];
+      FileO = argv[3];
+    }
+    MyMatrix<T> eSymmMat = ReadMatrixFile<T>(FileI);
     //
     int n = eSymmMat.rows();
-    std::cerr << "eSymmMat.rows=" << eSymmMat.rows()
-              << " eSymmMat.cols=" << eSymmMat.cols() << "\n";
     MyMatrix<Tint> TheBasis = IdentityMat<Tint>(n);
     ResultListCone<Tint> res =
         EnumerateListConeCopositive<T, Tint>(eSymmMat, TheBasis, std::cerr);
-    size_t nbCone = res.ListCone.size();
-    std::cerr << "nbCone=" << nbCone << "\n";
-    if (res.test == false) {
-      std::cerr << "Matrix is not Copositive\n";
-    } else {
-      std::ofstream OUTfs(argv[3]);
-      for (size_t iCone = 0; iCone < nbCone; iCone++) {
-        OUTfs << "iCone=" << iCone << "/" << nbCone << " Basis\n";
-        MyMatrix<Tint> const &eMat = res.ListCone[iCone];
-        WriteMatrix(OUTfs, eMat);
+    auto f_process=[&](std::ostream& os_out) -> void {
+      size_t nbCone = res.ListCone.size();
+      if (OutFormat == "CPP") {
+        if (res.test == false) {
+          os_out << "Matrix is not Copositive\n";
+        } else {
+          for (size_t iCone = 0; iCone < nbCone; iCone++) {
+            os_out << "iCone=" << iCone << "/" << nbCone << " Basis\n";
+            MyMatrix<Tint> const &eMat = res.ListCone[iCone];
+            WriteMatrix(os_out, eMat);
+          }
+        }
       }
-    }
+      if (OutFormat == "GAP") {
+        os_out << "return rec(";
+        if (res.test == false) {
+          os_out << "copositive:=false";
+        } else {
+          os_out << "copositive:=true";
+          os_out << ", ListCone:=";
+          WriteListMatrixGAP(os_out, res.ListCone);
+        }
+        os_out << ");\n";
+      }
+    };
+    print_stderr_stdout_file(FileO, f_process);
     std::cerr << "Normal completion of the program\n";
   } catch (TerminalException const &e) {
     std::cerr << "Error in CP_CopositiveListCone\n";
